6921e6dded3a7ce51d0b9edb24d2b2c76ad836fc
Goolnn <Goolnn@hotmail.com>
2024-10-20 17:45:19 +0800

    refactor: separate home page implementation into pages module

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,9 @@
 import 'package:cangyan/core/frb_generated.dart';
+import 'package:cangyan/pages/home.dart';
 import 'package:flutter/material.dart';
 
 Future<void> main() async {
   await RustLib.init();
 
   runApp(const MaterialApp(home: HomePage()));
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SafeArea(
-        child: Center(
-          child: Text('Hello, world!'),
-        ),
-      ),
-    );
-  }
 }
