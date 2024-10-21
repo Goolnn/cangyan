808d325bff80a727a56cb8222c555e0967b3f16e
Goolnn <Goolnn@hotmail.com>
2024-10-21 23:22:19 +0800

    feat(pages): define state field for home page

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,6 @@
+import 'package:cangyan/core/api/states/home.dart';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,8 +10,37 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  HomeState? state;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _initState();
+  }
+
+  Future<void> _initState() async {
+    final workspace = await getExternalStorageDirectory();
+
+    if (workspace == null) {
+      return;
+    }
+
+    setState(() {
+      state = HomeState(workspace: workspace.path);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    if (state == null) {
+      return const Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(),
+        ),
+      );
+    }
+
     return Scaffold(
       body: const SafeArea(
         child: Center(
