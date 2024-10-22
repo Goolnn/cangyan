1536559110d3a1936952926703b11d86889c1fbb
Goolnn <Goolnn@hotmail.com>
2024-10-22 13:34:44 +0800

    feat(core/states): remove file-loading code from constructor of home state

--- core/src/api/states/home.rs
+++ core/src/api/states/home.rs
@@ -13,28 +13,7 @@ impl HomeState {
     #[frb(sync)]
     pub fn new(workspace: String) -> Self {
         let workspace = PathBuf::from(workspace);
-
-        let mut files = Vec::new();
-
-        if let Ok(entries) = workspace.read_dir() {
-            for entry in entries.flatten() {
-                if !entry.path().is_file() {
-                    continue;
-                }
-
-                let stream = match fs::File::open(entry.path()) {
-                    Ok(stream) => stream,
-                    _ => continue,
-                };
-
-                let file = match File::open(stream) {
-                    Ok(file) => file,
-                    _ => continue,
-                };
-
-                files.push(file);
-            }
-        }
+        let files = Vec::new();
 
         HomeState { workspace, files }
     }
