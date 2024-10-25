b9814ff2ad684d656e929c8c6b33134e87dcaf5a
Goolnn <Goolnn@hotmail.com>
2024-10-25 21:58:29 +0800

    feat(core/states): implement some method for info state

--- core/src/api/states/info.rs
+++ core/src/api/states/info.rs
@@ -12,4 +12,67 @@ impl InfoState {
     pub fn new(file: Arc<File>) -> Self {
         InfoState { file }
     }
+
+    #[frb(sync)]
+    pub fn cover(&self) -> Option<Vec<u8>> {
+        None
+    }
+
+    #[frb(sync)]
+    pub fn page_count(&self) -> u32 {
+        self.file.project().pages().len() as u32
+    }
+
+    #[frb(sync)]
+    pub fn category(&self) -> String {
+        String::new()
+    }
+
+    #[frb(sync)]
+    pub fn title(&self) -> String {
+        self.file.project().title().to_string()
+    }
+
+    #[frb(sync)]
+    pub fn number(&self) -> (u32, u32) {
+        (0, 0)
+    }
+
+    #[frb(sync)]
+    pub fn created_date(&self) -> (u16, u8, u8, u8, u8, u8) {
+        let date = self.file.project().created_date();
+
+        (
+            date.year(),
+            date.month(),
+            date.day(),
+            date.hour(),
+            date.minute(),
+            date.second(),
+        )
+    }
+
+    #[frb(sync)]
+    pub fn saved_date(&self) -> (u16, u8, u8, u8, u8, u8) {
+        let date = self.file.project().saved_date();
+
+        (
+            date.year(),
+            date.month(),
+            date.day(),
+            date.hour(),
+            date.minute(),
+            date.second(),
+        )
+    }
+
+    #[frb(sync)]
+    pub fn pages(&self) -> Vec<Vec<u8>> {
+        self.file
+            .project()
+            .pages()
+            .iter()
+            .map(|page| page.data().to_owned())
+            .collect()
+    }
 }
