23d11ed57ea779476a0bcef7802e8615aaff1d8a
Goolnn <Goolnn@hotmail.com>
2024-10-27 11:02:12 +0800

    feat(widgets): extract parameters in image

--- lib/widgets/image.dart
+++ lib/widgets/image.dart
@@ -4,6 +4,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart' as flutter;
 
 class Image extends StatelessWidget {
+  final double aspectRatio = 3.0 / 4.0;
+  final double borderRadius = 8.0;
+
   final Uint8List? image;
 
   const Image({super.key, this.image});
@@ -11,15 +14,21 @@ class Image extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AspectRatio(
-      aspectRatio: 3.0 / 4.0,
+      aspectRatio: aspectRatio,
       child: image != null
           ? Center(
               child: ClipRRect(
-                borderRadius: BorderRadius.circular(8.0),
+                borderRadius: BorderRadius.circular(borderRadius),
                 child: flutter.Image.memory(image!),
               ),
             )
-          : const Icon(Icons.image_not_supported),
+          : Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(borderRadius),
+                color: Colors.grey.withOpacity(0.25),
+              ),
+              child: const Icon(Icons.image_not_supported),
+            ),
     );
   }
 }
