5cce672b1351640ad42ad029bb376d1001254e0d
Goolnn <Goolnn@hotmail.com>
2024-10-27 11:49:45 +0800

    feat(pages): wrap interactive viewer for image

--- lib/pages/edit.dart
+++ lib/pages/edit.dart
@@ -5,9 +5,13 @@ class EditPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SafeArea(
-        child: Placeholder(),
+        child: InteractiveViewer(
+          minScale: 0.1,
+          maxScale: 10.0,
+          child: const Placeholder(),
+        ),
       ),
     );
   }
