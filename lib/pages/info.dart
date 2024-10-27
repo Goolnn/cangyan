5dbca3174de3c6e6e85c9471dc1109461067e0e3
Goolnn <Goolnn@hotmail.com>
2024-10-27 09:58:54 +0800

    feat(pages): extract page widget in info page

--- lib/pages/info.dart
+++ lib/pages/info.dart
@@ -1,3 +1,5 @@
+import 'dart:typed_data';
+
 import 'package:cangyan/core/api/states/home.dart';
 import 'package:cangyan/core/api/states/info.dart';
 import 'package:flutter/material.dart';
@@ -41,29 +43,66 @@ class _InfoPageState extends State<InfoPage> {
                   ),
                   Wrap(
                     children: [
-                      for (int i = 0; i < pages.length; i++)
-                        FractionallySizedBox(
-                          widthFactor: 1.0 / 3.0,
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Column(
-                              children: [
-                                AspectRatio(
-                                  aspectRatio: 3.0 / 4.0,
-                                  child: Image.memory(pages[i]),
-                                ),
-                                const SizedBox(height: 4.0),
-                                Text('第${i + 1}页'),
-                              ],
-                            ),
-                          ),
-                        ),
+                      for (int index = 0; index < pages.length; index++)
+                        _Page(image: pages[index], index: index),
                     ],
                   ),
                 ],
               ),
             ),
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class _Page extends StatelessWidget {
+  final Uint8List image;
+  final int index;
+
+  const _Page({
+    required this.image,
+    required this.index,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return FractionallySizedBox(
+      widthFactor: 1.0 / 3.0,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            AspectRatio(
+              aspectRatio: 3.0 / 4.0,
+              child: Stack(
+                children: [
+                  Center(
+                    child: ClipRRect(
+                      borderRadius:
+                          const BorderRadius.all(Radius.circular(8.0)),
+                      child: Image.memory(image),
+                    ),
+                  ),
+                  Material(
+                    color: Colors.transparent,
+                    child: InkWell(
+                      focusColor: Colors.grey.withOpacity(0.15),
+                      hoverColor: Colors.grey.withOpacity(0.15),
+                      splashColor: Colors.grey.withOpacity(0.15),
+                      highlightColor: Colors.grey.withOpacity(0.15),
+                      borderRadius:
+                          const BorderRadius.all(Radius.circular(8.0)),
+                      onTap: () {},
+                    ),
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(height: 4.0),
+            Text('第${index + 1}页'),
+          ],
         ),
       ),
     );
