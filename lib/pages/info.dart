ca26943a3d75dd8293df66b256e36d93e445d6be
Goolnn <Goolnn@hotmail.com>
2024-10-27 11:30:29 +0800

    feat(pages): add tap event to navigate to edit page

--- lib/pages/info.dart
+++ lib/pages/info.dart
@@ -2,6 +2,7 @@ import 'dart:typed_data';
 
 import 'package:cangyan/core/api/states/home.dart';
 import 'package:cangyan/core/api/states/info.dart';
+import 'package:cangyan/pages/edit.dart';
 import 'package:cangyan/widgets/image.dart' as cangyan;
 import 'package:flutter/material.dart';
 
@@ -74,7 +75,31 @@ class _Page extends StatelessWidget {
         padding: const EdgeInsets.all(8.0),
         child: Column(
           children: [
-            cangyan.Image(image: image),
+            AspectRatio(
+              aspectRatio: 3.0 / 4.0,
+              child: Stack(
+                children: [
+                  cangyan.Image(image: image),
+                  Material(
+                    color: Colors.transparent,
+                    child: InkWell(
+                      focusColor: Colors.grey.withOpacity(0.15),
+                      highlightColor: Colors.grey.withOpacity(0.15),
+                      hoverColor: Colors.grey.withOpacity(0.15),
+                      splashColor: Colors.grey.withOpacity(0.15),
+                      borderRadius: BorderRadius.circular(8.0),
+                      onTap: () {
+                        Navigator.push(context, MaterialPageRoute(
+                          builder: (context) {
+                            return const EditPage();
+                          },
+                        ));
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
             const SizedBox(height: 4.0),
             Text('第${index + 1}页'),
           ],
