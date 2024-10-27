b9ff817c360666be0e707cfa0f5ca4a79a719f0d
Goolnn <Goolnn@hotmail.com>
2024-10-27 18:46:47 +0800

    feat(pages): reimplement edit page

--- lib/pages/edit.dart
+++ lib/pages/edit.dart
@@ -1,21 +1,82 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
 
 class EditPage extends StatefulWidget {
-  const EditPage({super.key});
+  final Uint8List image;
+
+  const EditPage({super.key, required this.image});
 
   @override
   State<EditPage> createState() => _EditPageState();
 }
 
 class _EditPageState extends State<EditPage> {
+  final TransformationController _controller = TransformationController();
+
+  double _scale = 1.0;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller.addListener(_onTransformChanged);
+  }
+
+  @override
+  void dispose() {
+    _controller.removeListener(_onTransformChanged);
+    _controller.dispose();
+
+    super.dispose();
+  }
+
+  void _onTransformChanged() {
+    setState(() {
+      _scale = _controller.value.getMaxScaleOnAxis();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: InteractiveViewer(
-          minScale: 0.1,
-          maxScale: 10.0,
-          child: const Placeholder(),
+        child: Stack(
+          children: [
+            Column(
+              children: [
+                Expanded(
+                  child: InteractiveViewer(
+                    transformationController: _controller,
+                    maxScale: 10.0,
+                    child: Image.memory(widget.image),
+                  ),
+                ),
+              ],
+            ),
+            if (_scale != 1.0)
+              Positioned(
+                top: 16.0,
+                left: 16.0,
+                child: Card(
+                  shape: const StadiumBorder(),
+                  color: Colors.black.withOpacity(0.15),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 12.0,
+                      vertical: 6.0,
+                    ),
+                    child: Text(
+                      '×${_scale.toStringAsFixed(2)}',
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+          ],
         ),
       ),
     );
