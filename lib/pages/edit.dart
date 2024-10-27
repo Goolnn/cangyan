f8144b6ab54ff9d3e70b60f6e0b4de934ee017df
Goolnn <Goolnn@hotmail.com>
2024-10-27 11:50:37 +0800

    feat(pages): convert edit page from stateless to stateful

--- lib/pages/edit.dart
+++ lib/pages/edit.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class EditPage extends StatelessWidget {
+class EditPage extends StatefulWidget {
   const EditPage({super.key});
 
+  @override
+  State<EditPage> createState() => _EditPageState();
+}
+
+class _EditPageState extends State<EditPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
