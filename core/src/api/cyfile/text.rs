5bdb4bf2f343ce4a110faf6d20603a62858914f4
Goolnn <Goolnn@hotmail.com>
2024-11-01 21:50:04 +0800

    fix(core/cyfile/text): change From implementation to accept reference

--- core/src/api/cyfile/text.rs
+++ core/src/api/cyfile/text.rs
@@ -6,8 +6,8 @@ pub struct Text {
     pub comment: String,
 }
 
-impl From<cyfile::Text> for Text {
-    fn from(text: cyfile::Text) -> Self {
+impl From<&cyfile::Text> for Text {
+    fn from(text: &cyfile::Text) -> Self {
         Text {
             content: text.content().to_string(),
             comment: text.comment().to_string(),
