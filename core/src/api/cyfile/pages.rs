b7d09fd640bfbb320963752f80d0d796b08bb399
Goolnn <Goolnn@hotmail.com>
2024-11-01 21:46:22 +0800

    feat(core/cyfile/pages): add method to get images of pages

--- core/src/api/cyfile/pages.rs
+++ core/src/api/cyfile/pages.rs
@@ -13,4 +13,22 @@ impl Pages {
     pub fn new(file: Arc<Mutex<File>>) -> Self {
         Pages { file }
     }
+
+    pub fn images(&self) -> anyhow::Result<Vec<Vec<u8>>> {
+        let file = self
+            .file
+            .lock()
+            .map_err(|e| anyhow::anyhow!(e.to_string()))?;
+
+        let project = file.project();
+        let pages = project.pages();
+
+        let mut images = Vec::new();
+
+        for page in pages {
+            images.push(page.data().to_owned());
+        }
+
+        Ok(images)
+    }
 }
