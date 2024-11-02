569151c4b53f5eda01bb44dbfd6c048a01d7ee79
Goolnn <Goolnn@hotmail.com>
2024-11-02 16:49:44 +0800

    feat(pages/info): add credits section with customizable capsules for contributors

--- lib/pages/info.dart
+++ lib/pages/info.dart
@@ -1,3 +1,4 @@
+import 'package:cangyan/core/api/cyfile/credit.dart';
 import 'package:cangyan/core/api/cyfile/summary.dart';
 import 'package:cangyan/widgets/widgets.dart' as cangyan;
 import 'package:flutter/material.dart';
@@ -22,22 +23,50 @@ class InfoPage extends StatelessWidget {
                 ),
               ),
               const Divider(),
-              SizedBox(
-                height: 32.0,
-                child: Row(
+              cangyan.Title(
+                summary.title(),
+                summary.number(),
+                textAlign: TextAlign.center,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
                   children: [
-                    cangyan.Category(summary.category()),
-                    Expanded(
-                      child: cangyan.Title(
-                        summary.title(),
-                        summary.number(),
-                        textAlign: TextAlign.center,
-                      ),
-                    ),
-                    cangyan.Progress(summary.progress()),
-                  ],
+                    ("作者", Credit.artists),
+                    ("翻译", Credit.translators),
+                    ("校对", Credit.proofreaders),
+                    ("修图", Credit.retouchers),
+                    ("嵌字", Credit.typesetters),
+                    ("监修", Credit.supervisors),
+                  ].map<Widget>((pair) {
+                    final names = summary.credits()[pair.$2] ?? <String>[];
+
+                    return Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        cangyan.Capsule(
+                          pair.$1,
+                          backgroundColor: Colors.blue,
+                        ),
+                        const SizedBox(width: 8.0),
+                        Expanded(
+                          child: Wrap(
+                            children: names.map<Widget>((credit) {
+                              return cangyan.Capsule(
+                                credit,
+                                backgroundColor: Colors.lightBlue,
+                              );
+                            }).toList()
+                              ..add(const SizedBox(width: 4.0))
+                              ..add(const cangyan.Capsule("+")),
+                          ),
+                        )
+                      ],
+                    );
+                  }).toList(),
                 ),
               ),
+              const Divider(),
             ],
           ),
         ),
