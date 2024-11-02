fd1d3901715635788ac843a56c855eda3c0546a7
Goolnn <Goolnn@hotmail.com>
2024-11-02 15:47:33 +0800

    refactor(widgets/image): remove aspectRatio and simplify image rendering logic

--- lib/widgets/image.dart
+++ lib/widgets/image.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart' as flutter;
 
 class Image extends StatelessWidget {
-  final double aspectRatio = 3.0 / 4.0;
   final double borderRadius = 8.0;
 
   final Uint8List? image;
@@ -13,22 +12,21 @@ class Image extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: aspectRatio,
-      child: image != null
-          ? Center(
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(borderRadius),
-                child: flutter.Image.memory(image!),
+    return image != null
+        ? Center(
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(borderRadius),
+              child: flutter.Image(
+                image: MemoryImage(image!),
               ),
-            )
-          : Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(borderRadius),
-                color: Colors.grey.withOpacity(0.25),
-              ),
-              child: const Icon(Icons.image_not_supported),
             ),
-    );
+          )
+        : Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(borderRadius),
+              color: Colors.grey.withOpacity(0.25),
+            ),
+            child: const Icon(Icons.image_not_supported),
+          );
   }
 }
