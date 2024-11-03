24500a9fc3fb3381eabd552698be63cce7cced5b
Goolnn <Goolnn@hotmail.com>
2024-11-03 09:38:37 +0800

    feat(main): set transparent status bar and adjust icon brightness

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
 import 'package:cangyan/core/frb_generated.dart';
 import 'package:cangyan/pages/home.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 Future<void> main() async {
   await RustLib.init();
+
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.dark,
+    ),
+  );
 
   runApp(
     const MaterialApp(
