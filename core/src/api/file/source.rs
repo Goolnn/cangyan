670dc04a0410cf603d854083b0633ce3076abd1b
Goolnn <Goolnn@hotmail.com>
2024-11-07 23:10:15 +0800

    feat(core/file/source): update Source struct to use Project and add open/create methods

--- core/src/api/file/source.rs
+++ core/src/api/file/source.rs
@@ -1,22 +1,31 @@
+use crate::api::file::Project;
 use cyfile::ExportArguments;
-use cyfile::File;
 use flutter_rust_bridge::frb;
+use std::path::Path;
 use std::path::PathBuf;
 
 #[frb(ignore)]
 pub struct Source {
-    pub file: File,
+    pub project: Project,
     pub path: PathBuf,
 }
 
 impl Source {
-    pub fn save(&self) -> anyhow::Result<()> {
-        self.file.export(ExportArguments::new(&self.path))
+    pub fn open(path: impl AsRef<Path>) -> anyhow::Result<Self> {
+        let project = Project::from(&cyfile::File::open(path.as_ref())?);
+        let path = path.as_ref().to_path_buf();
+
+        Ok(Self { project, path })
     }
 
-    pub fn delete(&self) -> anyhow::Result<()> {
-        std::fs::remove_file(&self.path)?;
-        
-        Ok(())
+    pub fn create(path: impl AsRef<Path>, project: Project) -> anyhow::Result<Self> {
+        cyfile::File::export(
+            &cyfile::Project::from(&project),
+            ExportArguments::new(path.as_ref()).with_version((0, 1)),
+        )?;
+
+        let path = path.as_ref().to_path_buf();
+
+        Ok(Self { project, path })
     }
 }
