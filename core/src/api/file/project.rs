963f7f9b4a410a23b349032a9d991268cd83ad90
Goolnn <Goolnn@hotmail.com>
2024-11-07 22:55:19 +0800

    feat(core/file/project): simplify Project struct by removing source and adding direct fields

--- core/src/api/file/project.rs
+++ core/src/api/file/project.rs
@@ -1,253 +1,49 @@
 use crate::api::file::Date;
 use crate::api::file::Page;
-use crate::api::file::Source;
 use cyfile::Credit;
 use flutter_rust_bridge::frb;
 use std::collections::HashMap;
 use std::collections::HashSet;
-use std::sync::Arc;
-use std::sync::Mutex;
 
-#[frb(opaque)]
+#[frb(non_opaque)]
 pub struct Project {
-    source: Arc<Mutex<Source>>,
+    pub cover: Vec<u8>,
+
+    pub category: String,
+    pub title: String,
+
+    pub number: (u32, u32),
+
+    pub comment: String,
+
+    pub created_date: Date,
+    pub updated_date: Date,
+
+    pub credits: HashMap<Credit, HashSet<String>>,
+
+    pub pages: Vec<Page>,
 }
 
-impl Project {
-    #[frb(ignore)]
-    pub fn new(source: Arc<Mutex<Source>>) -> Self {
-        Project { source }
-    }
+impl Project {}
 
-    #[frb(sync)]
-    pub fn set_cover(&mut self, cover: Vec<u8>) -> anyhow::Result<()> {
-        let mut source = self
-            .source
-            .lock()
-            .map_err(|e| anyhow::anyhow!(e.to_string()))?;
+impl From<&cyfile::Project> for Project {
+    fn from(value: &cyfile::Project) -> Self {
+        Self {
+            cover: value.cover().to_owned(),
 
-        source.file.project_mut().set_cover(cover);
+            category: value.category().to_string(),
+            title: value.title().to_string(),
 
-        source.save()?;
+            number: value.number(),
 
-        Ok(())
-    }
+            comment: value.comment().to_string(),
 
-    #[frb(sync)]
-    pub fn set_category(&mut self, category: String) -> anyhow::Result<()> {
-        let mut source = self
-            .source
-            .lock()
-            .map_err(|e| anyhow::anyhow!(e.to_string()))?;
+            created_date: Date::from(&value.created_date()),
+            updated_date: Date::from(&value.updated_data()),
 
-        source.file.project_mut().set_category(category);
+            credits: value.credits().clone(),
 
-        source.save()?;
-
-        Ok(())
-    }
-
-    #[frb(sync)]
-    pub fn set_title(&mut self, title: String) -> anyhow::Result<()> {
-        let mut source = self
-            .source
-            .lock()
-            .map_err(|e| anyhow::anyhow!(e.to_string()))?;
-
-        source.file.project_mut().set_title(title);
-
-        source.save()?;
-
-        Ok(())
-    }
-
-    #[frb(sync)]
-    pub fn set_number(&mut self, number: (u32, u32)) -> anyhow::Result<()> {
-        let mut source = self
-            .source
-            .lock()
-            .map_err(|e| anyhow::anyhow!(e.to_string()))?;
-
-        source.file.project_mut().set_number(number);
-
-        source.save()?;
-
-        Ok(())
-    }
-
-    #[frb(sync)]
-    pub fn set_credits(&mut self, credits: HashMap<Credit, HashSet<String>>) -> anyhow::Result<()> {
-        let mut source = self
-            .source
-            .lock()
-            .map_err(|e| anyhow::anyhow!(e.to_string()))?;
-
-        *source.file.project_mut().credits_mut() = credits;
-
-        source.save()?;
-
-        Ok(())
-    }
-
-    #[frb(sync)]
-    pub fn set_pages(&mut self, pages: Vec<Page>) -> anyhow::Result<()> {
-        let mut source = self
-            .source
-            .lock()
-            .map_err(|e| anyhow::anyhow!(e.to_string()))?;
-
-        *source.file.project_mut().pages_mut() = pages.iter().map(|page| page.into()).collect();
-
-        source.save()?;
-
-        Ok(())
-    }
-
-    #[frb(sync)]
-    pub fn cover(&self) -> anyhow::Result<Option<Vec<u8>>> {
-        let source = self
-            .source
-            .lock()
-            .map_err(|e| anyhow::anyhow!(e.to_string()))?;
-
-        let project = source.file.project();
-        let cover = project.cover();
-
-        if cover.is_empty() {
-            if project.pages().is_empty() {
-                Ok(None)
-            } else {
-                Ok(Some(project.pages()[0].data().to_owned()))
-            }
-        } else {
-            Ok(Some(cover.to_owned()))
+            pages: value.pages().iter().map(|page| Page::from(page)).collect(),
         }
-    }
-
-    #[frb(sync)]
-    pub fn category(&self) -> anyhow::Result<String> {
-        let source = self
-            .source
-            .lock()
-            .map_err(|e| anyhow::anyhow!(e.to_string()))?;
-
-        Ok(source.file.project().category().to_string())
-    }
-
-    #[frb(sync)]
-    pub fn title(&self) -> anyhow::Result<String> {
-        let source = self
-            .source
-            .lock()
-            .map_err(|e| anyhow::anyhow!(e.to_string()))?;
-
-        Ok(source.file.project().title().to_string())
-    }
-
-    #[frb(sync)]
-    pub fn number(&self) -> anyhow::Result<(u32, u32)> {
-        let source = self
-            .source
-            .lock()
-            .map_err(|e| anyhow::anyhow!(e.to_string()))?;
-
-        Ok(source.file.project().number())
-    }
-
-    #[frb(sync)]
-    pub fn credits(&self) -> anyhow::Result<HashMap<Credit, HashSet<String>>> {
-        let source = self
-            .source
-            .lock()
-            .map_err(|e| anyhow::anyhow!(e.to_string()))?;
-
-        Ok(source.file.project().credits().clone())
-    }
-
-    #[frb(sync)]
-    pub fn created_date(&self) -> anyhow::Result<Date> {
-        let source = self
-            .source
-            .lock()
-            .map_err(|e| anyhow::anyhow!(e.to_string()))?;
-
-        Ok(Date::from(source.file.project().created_date()))
-    }
-
-    #[frb(sync)]
-    pub fn saved_date(&self) -> anyhow::Result<Date> {
-        let source = self
-            .source
-            .lock()
-            .map_err(|e| anyhow::anyhow!(e.to_string()))?;
-
-        Ok(Date::from(source.file.project().saved_date()))
-    }
-
-    #[frb(sync)]
-    pub fn page_count(&self) -> anyhow::Result<u32> {
-        let source = self
-            .source
-            .lock()
-            .map_err(|e| anyhow::anyhow!(e.to_string()))?;
-
-        Ok(source.file.project().pages().len() as u32)
-    }
-
-    #[frb[sync]]
-    pub fn progress(&self) -> anyhow::Result<f64> {
-        let source = self
-            .source
-            .lock()
-            .map_err(|e| anyhow::anyhow!(e.to_string()))?;
-
-        let mut total = 0.0;
-        let mut completed = 0.0;
-
-        for page in source.file.project().pages() {
-            for note in page.notes() {
-                total += 1.0;
-
-                if note.choice() != 0 {
-                    completed += 1.0;
-                }
-            }
-        }
-
-        if total == 0.0 {
-            Ok(0.0)
-        } else {
-            Ok(completed / total)
-        }
-    }
-
-    #[frb(sync)]
-    pub fn pages(&self) -> anyhow::Result<Vec<Page>> {
-        let source = self
-            .source
-            .lock()
-            .map_err(|e| anyhow::anyhow!(e.to_string()))?;
-
-        let pages = source
-            .file
-            .project()
-            .pages()
-            .iter()
-            .map(|page| page.into())
-            .collect();
-
-        Ok(pages)
-    }
-
-    #[frb(sync)]
-    pub fn delete(&self) -> anyhow::Result<()> {
-        let mut source = self
-            .source
-            .lock()
-            .map_err(|e| anyhow::anyhow!(e.to_string()))?;
-
-        source.delete()?;
-
-        Ok(())
     }
 }
