c026cb2d1d00aa153af3c1102a4c432919a19a56
Goolnn <Goolnn@hotmail.com>
2024-11-07 23:00:04 +0800

    feat(core/file/project): implement From trait for Project to cyfile::Project conversion

--- core/src/api/file/project.rs
+++ core/src/api/file/project.rs
@@ -41,9 +41,28 @@ impl From<&cyfile::Project> for Project {
             created_date: Date::from(&value.created_date()),
             updated_date: Date::from(&value.updated_data()),
 
-            credits: value.credits().clone(),
+            credits: value.credits().to_owned(),
 
             pages: value.pages().iter().map(|page| Page::from(page)).collect(),
         }
+    }
+}
+
+impl From<&Project> for cyfile::Project {
+    fn from(value: &Project) -> Self {
+        Self::new()
+            .with_cover(value.cover.to_owned())
+            .with_category(value.category.to_string())
+            .with_title(value.title.to_string())
+            .with_number(value.number)
+            .with_comment(value.comment.to_string())
+            .with_credits(value.credits.to_owned())
+            .with_pages(
+                value
+                    .pages
+                    .iter()
+                    .map(|page| cyfile::Page::from(page))
+                    .collect(),
+            )
     }
 }
