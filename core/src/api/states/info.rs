52dad621c7ad303f6297f96b17d7fc0f35fed870
Goolnn <Goolnn@hotmail.com>
2024-11-10 21:34:48 +0800

    feat(core/states/info): enhance InfoState with file management and conversion from Summary

--- core/src/api/states/info.rs
+++ core/src/api/states/info.rs
@@ -1,1 +1,26 @@
-pub struct InfoState {}
+use crate::api::file::File;
+use crate::api::file::Summary;
+use flutter_rust_bridge::frb;
+use std::sync::Arc;
+use std::sync::Mutex;
+
+#[frb(opaque)]
+pub struct InfoState {
+    file: Arc<Mutex<File>>,
+}
+
+impl InfoState {
+    #[frb(sync)]
+    pub fn new(file: Arc<Mutex<File>>) -> Self {
+        InfoState { file }
+    }
+}
+
+#[frb]
+impl From<&Summary> for InfoState {
+    fn from(value: &Summary) -> Self {
+        Self {
+            file: value.file.clone(),
+        }
+    }
+}
