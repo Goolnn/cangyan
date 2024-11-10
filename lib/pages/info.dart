cba6d15685a131a510a10e90c8587f50d3537907
Goolnn <Goolnn@hotmail.com>
2024-11-10 23:24:53 +0800

    feat(pages/info): refactor InfoPage to use InfoState and update related logic

--- lib/pages/info.dart
+++ lib/pages/info.dart
@@ -1,29 +1,30 @@
 import 'package:cangyan/core/file.dart' as cangyan;
-import 'package:cangyan/pages/edit.dart';
+import 'package:cangyan/core/states.dart' as cangyan;
 import 'package:cangyan/widgets.dart' as cangyan;
 import 'package:flutter/material.dart';
 
 class InfoPage extends StatefulWidget {
-  final cangyan.Project project;
+  final cangyan.InfoState state;
 
-  const InfoPage({super.key, required this.project});
+  InfoPage({
+    super.key,
+    required cangyan.Summary summary,
+  }) : state = cangyan.InfoState.from(summary: summary);
 
   @override
   State<InfoPage> createState() => _InfoPageState();
 }
 
 class _InfoPageState extends State<InfoPage> {
-  late List<cangyan.Page> pages;
-
   @override
   void initState() {
     super.initState();
-
-    pages = widget.project.pages();
   }
 
   @override
   Widget build(BuildContext context) {
+    final summary = widget.state.summary();
+
     return Scaffold(
       body: SafeArea(
         child: Padding(
@@ -31,13 +32,12 @@ class _InfoPageState extends State<InfoPage> {
           child: SingleChildScrollView(
             child: Column(
               children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 48.0,
-                    vertical: 8.0,
-                  ),
-                  child: cangyan.Image(image: widget.project.cover()),
-                ),
+                const Padding(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 48.0,
+                      vertical: 8.0,
+                    ),
+                    child: Icon(Icons.image_not_supported)),
                 const Divider(),
                 Padding(
                   padding: const EdgeInsets.all(8.0),
@@ -46,14 +46,14 @@ class _InfoPageState extends State<InfoPage> {
                     children: [
                       Row(
                         children: [
-                          cangyan.Category(widget.project.category()),
+                          cangyan.Category(summary.category()),
                           Expanded(
                             child: cangyan.Title(
-                              widget.project.title(),
-                              widget.project.number(),
+                              summary.title(),
+                              summary.number(),
                             ),
                           ),
-                          cangyan.Progress(widget.project.progress()),
+                          // cangyan.Progress(summary.progress()),
                         ],
                       ),
                       Padding(
@@ -68,14 +68,14 @@ class _InfoPageState extends State<InfoPage> {
                         child: Column(
                           children: [
                             Text(
-                              '创建于 ${_dateToString(widget.project.createdDate())}',
+                              '创建于 ${_dateToString(summary.createdDate())}',
                               style: const TextStyle(
                                 fontSize: 12.0,
                                 color: Colors.grey,
                               ),
                             ),
                             Text(
-                              '修改于 ${_dateToString(widget.project.savedDate())}',
+                              '更新于 ${_dateToString(summary.updatedDate())}',
                               style: const TextStyle(
                                 fontSize: 12.0,
                                 color: Colors.grey,
@@ -99,8 +99,7 @@ class _InfoPageState extends State<InfoPage> {
                       ("嵌字", cangyan.Credit.typesetters),
                       ("监修", cangyan.Credit.supervisors),
                     ].map<Widget>((pair) {
-                      final names =
-                          widget.project.credits()[pair.$2] ?? <String>[];
+                      final names = summary.credits()[pair.$2] ?? <String>[];
 
                       return Row(
                         crossAxisAlignment: CrossAxisAlignment.start,
@@ -129,45 +128,45 @@ class _InfoPageState extends State<InfoPage> {
                     }).toList(),
                   ),
                 ),
-                const Divider(),
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Wrap(
-                    children: [
-                      for (int i = 0; i < pages.length; i++)
-                        FractionallySizedBox(
-                          widthFactor: 1.0 / 3.0,
-                          child: Column(
-                            children: [
-                              Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: GestureDetector(
-                                  behavior: HitTestBehavior.translucent,
-                                  onTap: () {
-                                    Navigator.push(
-                                      context,
-                                      MaterialPageRoute(builder: (context) {
-                                        return EditPage(
-                                          page: pages[i],
-                                        );
-                                      }),
-                                    );
-                                  },
-                                  child: AspectRatio(
-                                    aspectRatio: 3.0 / 4.0,
-                                    child: cangyan.Image(
-                                      image: pages[i].data,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              Text('第${i + 1}页'),
-                            ],
-                          ),
-                        ),
-                    ],
-                  ),
-                ),
+                // const Divider(),
+                // Align(
+                //   alignment: Alignment.centerLeft,
+                //   child: Wrap(
+                //     children: [
+                //       for (int i = 0; i < pages.length; i++)
+                //         FractionallySizedBox(
+                //           widthFactor: 1.0 / 3.0,
+                //           child: Column(
+                //             children: [
+                //               Padding(
+                //                 padding: const EdgeInsets.all(8.0),
+                //                 child: GestureDetector(
+                //                   behavior: HitTestBehavior.translucent,
+                //                   onTap: () {
+                //                     Navigator.push(
+                //                       context,
+                //                       MaterialPageRoute(builder: (context) {
+                //                         return EditPage(
+                //                           page: pages[i],
+                //                         );
+                //                       }),
+                //                     );
+                //                   },
+                //                   child: AspectRatio(
+                //                     aspectRatio: 3.0 / 4.0,
+                //                     child: cangyan.Image(
+                //                       image: pages[i].data,
+                //                     ),
+                //                   ),
+                //                 ),
+                //               ),
+                //               Text('第${i + 1}页'),
+                //             ],
+                //           ),
+                //         ),
+                //     ],
+                //   ),
+                // ),
               ],
             ),
           ),
