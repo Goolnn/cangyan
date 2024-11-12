d4c6fdfddc047c1b66e2fa8bd891196494c272aa
Goolnn <Goolnn@hotmail.com>
2024-11-12 16:35:47 +0800

    feat(pages/edit): enhance image rendering with dynamic sizing and interactive viewer

--- lib/pages/edit.dart
+++ lib/pages/edit.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:cangyan/core/states.dart' as cangyan;
 import 'package:cangyan/core/file.dart' as cangyan;
 import 'package:flutter/material.dart';
@@ -15,40 +17,89 @@ class EditPage extends StatefulWidget {
 }
 
 class _EditPageState extends State<EditPage> {
+  final viewerKey = GlobalKey();
+
   late cangyan.Page page;
+
+  Size? viewerSize;
+  Size? imageSize;
+  Size? pageSize;
 
   @override
   void initState() {
     super.initState();
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      final viewer = viewerKey.currentContext?.findRenderObject() as RenderBox;
+
+      setState(() {
+        viewerSize = viewer.size;
+      });
+    });
 
     page = widget.state.page()!;
   }
 
   @override
   Widget build(BuildContext context) {
+    final image = MemoryImage(page.data);
+
+    image.resolve(const ImageConfiguration()).addListener(
+      ImageStreamListener(
+        (info, _) {
+          setState(() {
+            imageSize = Size(
+              info.image.width.toDouble(),
+              info.image.height.toDouble(),
+            );
+          });
+        },
+      ),
+    );
+
     return Scaffold(
       body: SafeArea(
-        child: FutureBuilder(
-          future: precacheImage(
-            MemoryImage(page.data),
-            context,
-          ),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState != ConnectionState.done) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }
+        child: Stack(
+          children: [
+            Builder(
+              builder: (context) {
+                return InteractiveViewer(
+                  key: viewerKey,
+                  minScale: 1.0,
+                  maxScale: 10.0,
+                  child: Center(
+                    child: Image(
+                      image: image,
+                    ),
+                  ),
+                );
+              },
+            ),
+            Builder(
+              builder: (context) {
+                if (viewerSize == null || imageSize == null) {
+                  return Container();
+                }
 
-            return InteractiveViewer(
-              maxScale: 10.0,
-              child: Center(
-                child: Image.memory(
-                  page.data,
-                ),
-              ),
-            );
-          },
+                final widthRatio = viewerSize!.width / imageSize!.width;
+                final heightRatio = viewerSize!.height / imageSize!.height;
+
+                final scale = min(widthRatio, heightRatio);
+
+                pageSize = Size(
+                  imageSize!.width * scale,
+                  imageSize!.height * scale,
+                );
+
+                return Center(
+                  child: SizedBox.fromSize(
+                    size: pageSize,
+                    child: const Placeholder(),
+                  ),
+                );
+              },
+            ),
+          ],
         ),
       ),
     );
