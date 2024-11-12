d2cf1c6500cb7454238134efe80b448b3e422cb1
Goolnn <Goolnn@hotmail.com>
2024-11-12 22:52:48 +0800

    feat(core/states/edit): add methods to append and remove notes in the edit state

--- core/src/api/states/edit.rs
+++ core/src/api/states/edit.rs
@@ -1,4 +1,5 @@
 use crate::api::cyfile::File;
+use crate::api::cyfile::Note;
 use crate::api::cyfile::Page;
 use flutter_rust_bridge::frb;
 use std::sync::Arc;
@@ -25,6 +26,51 @@ impl EditState {
             .map_err(|e| anyhow::anyhow!(e.to_string()))?;
 
         file.project.pages[self.page_index] = page;
+
+        file.save()?;
+
+        Ok(())
+    }
+
+    #[frb(sync)]
+    pub fn append_note(&self, x: f64, y: f64) -> anyhow::Result<()> {
+        let mut file = self
+            .file
+            .lock()
+            .map_err(|e| anyhow::anyhow!(e.to_string()))?;
+
+        let page = file
+            .project
+            .pages
+            .get_mut(self.page_index)
+            .ok_or_else(|| anyhow::anyhow!("page index out of bounds: {}", self.page_index))?;
+
+        page.notes.push(Note {
+            x,
+            y,
+            choice: 0,
+            texts: Vec::new(),
+        });
+
+        file.save()?;
+
+        Ok(())
+    }
+
+    #[frb(sync)]
+    pub fn remove_note(&self, note_index: usize) -> anyhow::Result<()> {
+        let mut file = self
+            .file
+            .lock()
+            .map_err(|e| anyhow::anyhow!(e.to_string()))?;
+
+        let page = file
+            .project
+            .pages
+            .get_mut(self.page_index)
+            .ok_or_else(|| anyhow::anyhow!("page index out of bounds: {}", self.page_index))?;
+
+        page.notes.remove(note_index);
 
         file.save()?;
 
