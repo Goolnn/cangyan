67b82b28168d952cdf77b575720d3a37e900d868
Goolnn <Goolnn@hotmail.com>
2024-11-12 21:10:16 +0800

    feat(pages/edit): enhance PopScope behavior to account for editing state during pop actions

--- lib/pages/edit.dart
+++ lib/pages/edit.dart
@@ -218,7 +218,7 @@ class _EditPageState extends State<EditPage> {
                 return PopScope(
                   canPop: false,
                   onPopInvokedWithResult: (didPop, result) {
-                    if (didPop) {
+                    if (didPop || editing) {
                       return;
                     }
 
