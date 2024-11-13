7c69498f5ed4e807473b73468b22c90ecee1ca94
Goolnn <Goolnn@hotmail.com>
2024-11-13 21:36:23 +0800

    feat(core/states): export CreateState for project creation

--- lib/core/states.dart
+++ lib/core/states.dart
@@ -1,3 +1,4 @@
+export 'package:cangyan/core/api/states/create.dart';
 export 'package:cangyan/core/api/states/edit.dart';
 export 'package:cangyan/core/api/states/home.dart';
 export 'package:cangyan/core/api/states/info.dart';
