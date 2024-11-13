259f2559e224b6fb8bf62dae9297364b64d8d940
Goolnn <Goolnn@hotmail.com>
2024-11-13 12:55:33 +0800

    feat(widgets/mark): add long press gesture support to Mark widget

--- lib/widgets/mark.dart
+++ lib/widgets/mark.dart
@@ -7,56 +7,61 @@ class Mark extends StatelessWidget {
   final int index;
   final bool isDone;
   final void Function()? onPressed;
+  final void Function()? onLongPressed;
 
   const Mark({
     super.key,
     required this.index,
     this.isDone = false,
     this.onPressed,
+    this.onLongPressed,
   });
 
   @override
   Widget build(BuildContext context) {
     return Transform.translate(
       offset: const Offset(-size, -size),
-      child: SizedBox.fromSize(
-        size: const Size.fromRadius(size),
-        child: TweenAnimationBuilder(
-          duration: duration,
-          tween: ColorTween(
-            begin: Colors.red,
-            end: isDone ? Colors.lightBlue : Colors.red,
+      child: GestureDetector(
+        onLongPress: onLongPressed,
+        child: SizedBox.fromSize(
+          size: const Size.fromRadius(size),
+          child: TweenAnimationBuilder(
+            duration: duration,
+            tween: ColorTween(
+              begin: Colors.red,
+              end: isDone ? Colors.lightBlue : Colors.red,
+            ),
+            builder: (context, value, child) {
+              return FilledButton(
+                style: ButtonStyle(
+                  elevation: const WidgetStatePropertyAll(8.0),
+                  shape: const WidgetStatePropertyAll(CircleBorder()),
+                  backgroundColor: WidgetStatePropertyAll(value),
+                  padding: const WidgetStatePropertyAll(EdgeInsets.zero),
+                ),
+                onPressed: onPressed,
+                child: AnimatedSwitcher(
+                  duration: duration,
+                  transitionBuilder: (child, animation) {
+                    return FadeTransition(
+                      opacity: animation,
+                      child: child,
+                    );
+                  },
+                  child: isDone
+                      ? const Icon(
+                          Icons.check,
+                          color: Colors.white,
+                          size: 16.0,
+                        )
+                      : Text(
+                          '$index',
+                          style: const TextStyle(color: Colors.white),
+                        ),
+                ),
+              );
+            },
           ),
-          builder: (context, value, child) {
-            return FilledButton(
-              style: ButtonStyle(
-                elevation: const WidgetStatePropertyAll(8.0),
-                shape: const WidgetStatePropertyAll(CircleBorder()),
-                backgroundColor: WidgetStatePropertyAll(value),
-                padding: const WidgetStatePropertyAll(EdgeInsets.zero),
-              ),
-              onPressed: onPressed,
-              child: AnimatedSwitcher(
-                duration: duration,
-                transitionBuilder: (child, animation) {
-                  return FadeTransition(
-                    opacity: animation,
-                    child: child,
-                  );
-                },
-                child: isDone
-                    ? const Icon(
-                        Icons.check,
-                        color: Colors.white,
-                        size: 16.0,
-                      )
-                    : Text(
-                        '$index',
-                        style: const TextStyle(color: Colors.white),
-                      ),
-              ),
-            );
-          },
         ),
       ),
     );
