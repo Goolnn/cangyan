2d5adf8e3061a1fd23cebbb2339d190f6a61baf0
Goolnn <Goolnn@hotmail.com>
2024-11-13 19:12:06 +0800

    feat(pages/info): add long press menu for page insertion and deletion with image selection

--- lib/pages/info.dart
+++ lib/pages/info.dart
@@ -3,6 +3,7 @@ import 'package:cangyan/core/states.dart' as cangyan;
 import 'package:cangyan/pages/edit.dart';
 import 'package:cangyan/widgets.dart' as cangyan;
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class InfoPage extends StatefulWidget {
   final cangyan.InfoState state;
@@ -17,6 +18,8 @@ class InfoPage extends StatefulWidget {
 }
 
 class _InfoPageState extends State<InfoPage> {
+  static const platform = MethodChannel('goolnn.cangyan/intent');
+
   @override
   void initState() {
     super.initState();
@@ -155,6 +158,82 @@ class _InfoPageState extends State<InfoPage> {
                                         );
                                       }),
                                     );
+                                  },
+                                  onLongPressStart: (details) {
+                                    showMenu<int>(
+                                      context: context,
+                                      position: RelativeRect.fromLTRB(
+                                        details.globalPosition.dx,
+                                        details.globalPosition.dy,
+                                        details.globalPosition.dx,
+                                        details.globalPosition.dy,
+                                      ),
+                                      items: [
+                                        const PopupMenuItem(
+                                          value: 1,
+                                          child: Text("向前插入新页"),
+                                        ),
+                                        const PopupMenuItem(
+                                          value: 2,
+                                          child: Text("向后插入新页"),
+                                        ),
+                                        const PopupMenuDivider(),
+                                        const PopupMenuItem(
+                                          value: 3,
+                                          child: Text("删除页面"),
+                                        ),
+                                      ],
+                                    ).then((value) async {
+                                      if (value case 1) {
+                                        final images = (await platform
+                                                .invokeListMethod("openImages"))
+                                            ?.map((image) => image as Uint8List)
+                                            .toList();
+
+                                        if (images == null) {
+                                          return;
+                                        }
+
+                                        for (final image in images.reversed) {
+                                          widget.state.insertPageBefore(
+                                            index: BigInt.from(i),
+                                            image: image,
+                                          );
+
+                                          setState(() {
+                                            pages.insert(i, image);
+                                          });
+                                        }
+                                      } else if (value case 2) {
+                                        final images = (await platform
+                                                .invokeListMethod("openImages"))
+                                            ?.map((image) => image as Uint8List)
+                                            .toList();
+
+                                        if (images == null) {
+                                          return;
+                                        }
+
+                                        for (final image in images.reversed) {
+                                          widget.state.insertPageAfter(
+                                            index: BigInt.from(i),
+                                            image: image,
+                                          );
+
+                                          setState(() {
+                                            pages.insert(i + 1, image);
+                                          });
+                                        }
+                                      } else if (value case 3) {
+                                        widget.state.removePage(
+                                          index: BigInt.from(i),
+                                        );
+
+                                        setState(() {
+                                          pages.removeAt(i);
+                                        });
+                                      }
+                                    });
                                   },
                                   child: AspectRatio(
                                     aspectRatio: 3.0 / 4.0,
