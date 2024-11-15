f0129a7d51e144a12017620003a6c57b43019aea
Goolnn <Goolnn@hotmail.com>
2024-11-15 14:56:21 +0800

    feat(main): add platform-specific workspace directory handling

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:cangyan/core/frb_generated.dart';
 import 'package:cangyan/pages/home.dart';
 import 'package:flutter/material.dart';
@@ -7,16 +9,24 @@ import 'package:path_provider/path_provider.dart';
 Future<void> main() async {
   await RustLib.init();
 
-  SystemChrome.setSystemUIOverlayStyle(
-    const SystemUiOverlayStyle(
-      statusBarColor: Colors.transparent,
-      statusBarIconBrightness: Brightness.dark,
-    ),
-  );
+  if (Platform.isAndroid) {
+    SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+        statusBarIconBrightness: Brightness.dark,
+      ),
+    );
+  }
 
   WidgetsFlutterBinding.ensureInitialized();
 
-  final workspace = await getExternalStorageDirectory();
+  String? workspace;
+
+  if (Platform.isWindows) {
+    workspace = "${(await getApplicationDocumentsDirectory()).path}/cangyan";
+  } else if (Platform.isAndroid) {
+    workspace = (await getExternalStorageDirectory())?.path;
+  }
 
   if (workspace == null) {
     return;
@@ -26,7 +36,7 @@ Future<void> main() async {
     MaterialApp(
       title: "苍眼",
       home: HomePage(
-        workspace: workspace.path,
+        workspace: workspace,
       ),
     ),
   );
