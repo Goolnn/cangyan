4de15d2fe67b03170ed8257c968f6cac8d8204fb
Goolnn <Goolnn@hotmail.com>
2024-11-15 17:49:49 +0800

    feat(widgets/input): add placeholder support to Input widget

--- lib/widgets/input.dart
+++ lib/widgets/input.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
 
 class Input extends StatelessWidget {
-  const Input({super.key});
+  final String? placeholder;
+
+  const Input({
+    super.key,
+    this.placeholder,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return TextField(
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+        isDense: true,
+        isCollapsed: true,
+        contentPadding: const EdgeInsets.all(10.0),
+        hintText: placeholder,
+        hintStyle: const TextStyle(
+          color: Colors.grey,
+        ),
+      ),
+      style: Theme.of(context).textTheme.bodyMedium,
+    );
   }
 }
