7f758ca4c92a4b5b47a0a46a8a2df86a4788a431
Goolnn <Goolnn@hotmail.com>
2024-11-15 18:56:32 +0800

    feat(main): initialize window manager and set window options

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,24 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:window_manager/window_manager.dart';
 
 Future<void> main() async {
   await RustLib.init();
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await windowManager.ensureInitialized();
+
+  WindowOptions options = const WindowOptions(
+    size: Size(960, 720),
+    minimumSize: Size(640, 480),
+    center: true,
+  );
+
+  windowManager.waitUntilReadyToShow(options, () async {
+    await windowManager.show();
+  });
 
   if (Platform.isAndroid) {
     SystemChrome.setSystemUIOverlayStyle(
@@ -18,8 +33,6 @@ Future<void> main() async {
       ),
     );
   }
-
-  WidgetsFlutterBinding.ensureInitialized();
 
   String? workspace;
 
