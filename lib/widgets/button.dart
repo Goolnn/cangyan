19708a7b6d12812e9077ba7f175d9b279d8d6bd0
Goolnn <Goolnn@hotmail.com>
2024-11-15 19:18:04 +0800

    feat(widgets/button): implement customizable Button widget with styling and functionality

--- lib/widgets/button.dart
+++ lib/widgets/button.dart
@@ -1,10 +1,40 @@
 import 'package:flutter/material.dart';
 
 class Button extends StatelessWidget {
-  const Button({super.key});
+  final String text;
+
+  const Button(
+    this.text, {
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return RawMaterialButton(
+      elevation: 0.0,
+      focusElevation: 0.0,
+      hoverElevation: 0.0,
+      disabledElevation: 0.0,
+      highlightElevation: 0.0,
+      constraints: const BoxConstraints(
+        minWidth: 72.0,
+        minHeight: 32.0,
+      ),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8.0),
+        side: const BorderSide(
+          color: Colors.grey,
+          width: 1.0,
+        ),
+      ),
+      fillColor: Colors.white,
+      padding: const EdgeInsets.symmetric(
+        horizontal: 8.0,
+        vertical: 4.0,
+      ),
+      materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+      onPressed: () {},
+      child: Text(text),
+    );
   }
 }
