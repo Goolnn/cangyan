1a6256d660b2f93c338099b4f984aa380a93c2c3
Goolnn <Goolnn@hotmail.com>
2024-11-16 23:06:16 +0800

    feat(core/states/workspace): add load method to retrieve and manage project files

--- core/src/api/states/workspace.rs
+++ core/src/api/states/workspace.rs
@@ -1,3 +1,4 @@
+use crate::api::cyfile::Project;
 use flutter_rust_bridge::frb;
 use std::path::PathBuf;
 
@@ -13,4 +14,37 @@ impl Workspace {
 
         Workspace { path }
     }
+
+    pub fn load(&self) -> anyhow::Result<Vec<File>> {
+        let mut files = Vec::new();
+
+        if let Ok(entries) = self.path.read_dir() {
+            for entry in entries.flatten() {
+                if !entry.path().is_file() {
+                    continue;
+                }
+
+                let path = entry.path();
+                let file = cyfile::File::open(path.as_path());
+
+                if let Ok(file) = file {
+                    let project = Project::from(&file);
+
+                    files.push(File { project, path });
+                } else {
+                    std::fs::remove_file(path)?;
+                }
+            }
+        }
+
+        Ok(files)
+    }
+}
+
+#[frb]
+pub struct File {
+    #[frb(non_final)]
+    pub project: Project,
+    #[frb(ignore)]
+    pub path: PathBuf,
 }
