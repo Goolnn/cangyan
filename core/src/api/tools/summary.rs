741bb3e9055f07992d25874a0bc163da5b796ef4
Goolnn <Goolnn@hotmail.com>
2024-11-17 16:58:08 +0800

    feat(cire/tools/summary): add methods for project metadata retrieval and date conversion

--- core/src/api/tools/summary.rs
+++ core/src/api/tools/summary.rs
@@ -1,4 +1,5 @@
-use cyfile::File;
+use crate::api::cyfile::Date;
+use crate::api::tools::File;
 use flutter_rust_bridge::frb;
 use std::sync::Arc;
 use std::sync::Mutex;
@@ -11,5 +12,93 @@ impl Summary {
     #[frb(sync)]
     pub fn new(file: Arc<Mutex<File>>) -> Self {
         Summary { file }
+    }
+
+    pub fn cover(&self) -> anyhow::Result<Vec<u8>> {
+        let file = self
+            .file
+            .lock()
+            .map_err(|e| anyhow::anyhow!(e.to_string()))?;
+
+        let cover = file.project.cover().to_owned();
+
+        Ok(cover)
+    }
+
+    pub fn page_count(&self) -> anyhow::Result<usize> {
+        let file = self
+            .file
+            .lock()
+            .map_err(|e| anyhow::anyhow!(e.to_string()))?;
+
+        let page_count = file.project.pages().len();
+
+        Ok(page_count)
+    }
+
+    pub fn category(&self) -> anyhow::Result<String> {
+        let file = self
+            .file
+            .lock()
+            .map_err(|e| anyhow::anyhow!(e.to_string()))?;
+
+        let category = file.project.category().to_owned();
+
+        Ok(category)
+    }
+
+    pub fn title(&self) -> anyhow::Result<String> {
+        let file = self
+            .file
+            .lock()
+            .map_err(|e| anyhow::anyhow!(e.to_string()))?;
+
+        let title = file.project.title().to_owned();
+
+        Ok(title)
+    }
+
+    pub fn number(&self) -> anyhow::Result<(u32, u32)> {
+        let file = self
+            .file
+            .lock()
+            .map_err(|e| anyhow::anyhow!(e.to_string()))?;
+
+        let number = file.project.number();
+
+        Ok(number)
+    }
+
+    pub fn comment(&self) -> anyhow::Result<String> {
+        let file = self
+            .file
+            .lock()
+            .map_err(|e| anyhow::anyhow!(e.to_string()))?;
+
+        let comment = file.project.comment().to_owned();
+
+        Ok(comment)
+    }
+
+    pub fn created_date(&self) -> anyhow::Result<Date> {
+        let file = self
+            .file
+            .lock()
+            .map_err(|e| anyhow::anyhow!(e.to_string()))?;
+
+        let created_date = Date::from(file.project.created_date());
+
+        Ok(created_date)
+    }
+
+    pub fn updated_date(&self) -> anyhow::Result<Date> {
+        let file = self
+            .file
+            .lock()
+            .map_err(|e| anyhow::anyhow!(e.to_string()))?;
+
+        let updated_date = Date::from(file.project.updated_data());
+
+        Ok(updated_date)
     }
 }
