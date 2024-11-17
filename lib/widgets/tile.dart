5c3d1467ece7d5ac0bfda268d14f49d5acf89ac3
Goolnn <Goolnn@hotmail.com>
2024-11-17 22:30:14 +0800

    feat(widgets/tile): convert Tile widget to StatefulWidget and add summary parameter

--- lib/widgets/tile.dart
+++ lib/widgets/tile.dart
@@ -1,8 +1,19 @@
+import 'package:cangyan/core.dart' as cangyan;
 import 'package:flutter/material.dart';
 
-class Tile extends StatelessWidget {
-  const Tile({super.key});
+class Tile extends StatefulWidget {
+  final cangyan.Summary summary;
 
+  const Tile({
+    super.key,
+    required this.summary,
+  });
+
+  @override
+  State<Tile> createState() => _TileState();
+}
+
+class _TileState extends State<Tile> {
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
