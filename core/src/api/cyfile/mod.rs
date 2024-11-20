ac57f139f3f70e979c9e1c22ff1e3d94a786e06d
Goolnn <Goolnn@hotmail.com>
2024-11-20 23:06:15 +0800

    feat(core/cyfile): remove unused Page and Project modules

--- core/src/api/cyfile/mod.rs
+++ core/src/api/cyfile/mod.rs
@@ -1,11 +1,7 @@
 pub mod date;
 pub mod note;
-pub mod page;
-pub mod project;
 pub mod text;
 
 pub use date::Date;
 pub use note::Note;
-pub use page::Page;
-pub use project::Project;
 pub use text::Text;
