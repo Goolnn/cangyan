e3a610087655f4061bef1965bc5acf0685c67537
Goolnn <Goolnn@hotmail.com>
2024-11-20 22:49:35 +0800

    feat(core/tools/editor): add methods to update note position, content, and comment

--- core/src/api/tools/editor.rs
+++ core/src/api/tools/editor.rs
@@ -16,6 +16,58 @@ impl Editor {
         Editor { file, index }
     }
 
+    #[frb(sync)]
+    pub fn update_note_position(&self, index: usize, x: f64, y: f64) -> anyhow::Result<()> {
+        let mut file = self
+            .file
+            .lock()
+            .map_err(|e| anyhow::anyhow!(e.to_string()))?;
+
+        let page = &mut file.project.pages_mut()[self.index];
+        let note = &mut page.notes_mut()[index];
+
+        note.set_x(x);
+        note.set_y(y);
+
+        file.save()?;
+
+        Ok(())
+    }
+
+    #[frb(sync)]
+    pub fn update_note_content(&self, index: usize, content: String) -> anyhow::Result<()> {
+        let mut file = self
+            .file
+            .lock()
+            .map_err(|e| anyhow::anyhow!(e.to_string()))?;
+
+        let page = &mut file.project.pages_mut()[self.index];
+        let note = &mut page.notes_mut()[index];
+
+        note.texts_mut()[0].set_content(content);
+
+        file.save()?;
+
+        Ok(())
+    }
+
+    #[frb(sync)]
+    pub fn update_note_comment(&self, index: usize, comment: String) -> anyhow::Result<()> {
+        let mut file = self
+            .file
+            .lock()
+            .map_err(|e| anyhow::anyhow!(e.to_string()))?;
+
+        let page = &mut file.project.pages_mut()[self.index];
+        let note = &mut page.notes_mut()[index];
+
+        note.texts_mut()[0].set_comment(comment);
+
+        file.save()?;
+
+        Ok(())
+    }
+
     pub fn notes(&self) -> anyhow::Result<Vec<Note>> {
         let file = self
             .file
