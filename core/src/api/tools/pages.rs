0a2c785f5426451dabc50c49b705db4c841a771b
Goolnn <Goolnn@hotmail.com>
2024-11-20 22:24:46 +0800

    feat(core/tools/pages): add methods to insert and remove pages, and retrieve images

--- core/src/api/tools/pages.rs
+++ core/src/api/tools/pages.rs
@@ -1,4 +1,5 @@
 use crate::api::tools::File;
+use cyfile::Page;
 use flutter_rust_bridge::frb;
 use std::sync::Arc;
 use std::sync::Mutex;
@@ -11,5 +12,53 @@ impl Pages {
     #[frb(sync)]
     pub fn new(file: Arc<Mutex<File>>) -> Self {
         Pages { file }
+    }
+
+    #[frb(sync)]
+    pub fn insert_page_before(&self, index: usize, image: Vec<u8>) -> anyhow::Result<()> {
+        let mut file = self
+            .file
+            .lock()
+            .map_err(|e| anyhow::anyhow!(e.to_string()))?;
+
+        file.project.pages_mut().insert(index, Page::new(image));
+
+        file.save()?;
+
+        Ok(())
+    }
+
+    #[frb(sync)]
+    pub fn insert_page_after(&self, index: usize, image: Vec<u8>) -> anyhow::Result<()> {
+        self.insert_page_before(index + 1, image)
+    }
+
+    #[frb(sync)]
+    pub fn remove_page(&self, index: usize) -> anyhow::Result<()> {
+        let mut file = self
+            .file
+            .lock()
+            .map_err(|e| anyhow::anyhow!(e.to_string()))?;
+
+        file.project.pages_mut().remove(index);
+
+        file.save()?;
+
+        Ok(())
+    }
+
+    #[frb(sync)]
+    pub fn images(&self) -> anyhow::Result<Vec<Vec<u8>>> {
+        let file = self
+            .file
+            .lock()
+            .map_err(|e| anyhow::anyhow!(e.to_string()))?;
+
+        Ok(file
+            .project
+            .pages()
+            .iter()
+            .map(|page| page.data().to_vec())
+            .collect())
     }
 }
