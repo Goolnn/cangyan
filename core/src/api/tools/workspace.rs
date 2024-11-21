e25ef978024771b1990fea69bd9fd81a30a6e75d
Goolnn <Goolnn@hotmail.com>
2024-11-21 09:18:40 +0800

    feat(workspace): update create and import methods to return Summary instead of File

--- core/src/api/tools/workspace.rs
+++ core/src/api/tools/workspace.rs
@@ -56,7 +56,7 @@ impl Workspace {
         Ok(summaries)
     }
 
-    pub fn create(&self, title: String, images: Vec<Vec<u8>>) -> anyhow::Result<File> {
+    pub fn create(&self, title: String, images: Vec<Vec<u8>>) -> anyhow::Result<Summary> {
         let path = self.path.join(format!("{}.cy", title));
 
         let pages = images.into_iter().map(cyfile::Page::new).collect();
@@ -67,20 +67,22 @@ impl Workspace {
             ExportArguments::new(path.as_path()).with_version((0, 1)),
         )?;
 
-        let file = File { project, path };
+        let file = Arc::new(Mutex::new(File { project, path }));
+        let summary = Summary::new(file);
 
-        Ok(file)
+        Ok(summary)
     }
 
-    pub fn import(&self, title: String, data: Vec<u8>) -> anyhow::Result<Arc<Mutex<File>>> {
+    pub fn import(&self, title: String, data: Vec<u8>) -> anyhow::Result<Summary> {
         let path = self.path.join(format!("{}.cy", title));
 
         std::fs::write(path.as_path(), data)?;
 
         let project = cyfile::File::open(path.as_path())?;
         let file = Arc::new(Mutex::new(File { project, path }));
+        let summary = Summary::new(file);
 
-        Ok(file)
+        Ok(summary)
     }
 
     #[frb(sync)]
