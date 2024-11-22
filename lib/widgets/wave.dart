942fad34ac78d0f0a4e5ef52d457f04ba531f6e9
Goolnn <Goolnn@hotmail.com>
2024-11-22 21:22:43 +0800

    feat(widgets/wave): replace onPress with onTap and enhance long press handling

--- lib/widgets/wave.dart
+++ lib/widgets/wave.dart
@@ -3,37 +3,45 @@ import 'package:flutter/material.dart';
 class Wave extends StatelessWidget {
   final BorderRadius? borderRadius;
 
-  final Function()? onPress;
+  final Function()? onTap;
   final Function()? onLongPress;
+  final Function(LongPressStartDetails details)? onLongPressStart;
+  final Function(LongPressEndDetails details)? onLongPressEnd;
 
   final Widget child;
 
   const Wave({
     super.key,
     this.borderRadius,
-    this.onPress,
+    this.onTap,
     this.onLongPress,
+    this.onLongPressStart,
+    this.onLongPressEnd,
     required this.child,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        child,
-        Positioned.fill(
-          child: Material(
-            color: Colors.transparent,
-            child: InkWell(
-              borderRadius: borderRadius,
-              splashColor: Colors.black.withOpacity(0.08),
-              highlightColor: Colors.black.withOpacity(0.08),
-              onTap: onPress,
-              onLongPress: onLongPress,
+    return GestureDetector(
+      onLongPress: onLongPress,
+      onLongPressStart: onLongPressStart,
+      onLongPressEnd: onLongPressEnd,
+      child: Stack(
+        children: [
+          child,
+          Positioned.fill(
+            child: Material(
+              color: Colors.transparent,
+              child: InkWell(
+                onTap: onTap,
+                borderRadius: borderRadius,
+                splashColor: Colors.black.withOpacity(0.08),
+                highlightColor: Colors.black.withOpacity(0.08),
+              ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
