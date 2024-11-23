c898157e0731a9f1faf8facfb813e0ca98d74304
Goolnn <Goolnn@hotmail.com>
2024-11-23 19:34:41 +0800

    feat(pages/duplicated): refactor dialog layout for improved UI and usability

--- lib/pages/duplicated.dart
+++ lib/pages/duplicated.dart
@@ -36,79 +36,189 @@ class _DuplicatedPageState extends State<DuplicatedPage> {
       },
     ).toList();
 
-    return AlertDialog(
-      title: const Text('工程重名'),
-      contentPadding: const EdgeInsets.symmetric(
-        horizontal: 24.0,
-        vertical: 8.0,
+    return Dialog(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(12.0),
       ),
-      content: ConstrainedBox(
-        constraints: const BoxConstraints(
-          maxHeight: 128.0,
-        ),
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              for (var i = 0; i < pairs.length; i++)
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 4.0),
-                  child: Row(
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                '工程重名',
+                style: TextStyle(fontSize: 20.0),
+              ),
+            ),
+            const Divider(),
+            Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: ConstrainedBox(
+                constraints: const BoxConstraints(
+                  maxHeight: 128.0,
+                ),
+                child: SingleChildScrollView(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
                     children: [
-                      Icon(
-                        duplicateds[i]
-                            ? Icons.error_outline
-                            : Icons.check_circle_outline,
-                        color: duplicateds[i] ? Colors.red : Colors.green,
-                      ),
-                      const SizedBox(width: 8.0),
-                      Expanded(
-                        child: cangyan.EditableText(
-                          pairs[i].$1,
-                          onSubmitted: (text) {
-                            setState(() {
-                              pairs[i] = (text, pairs[i].$2);
-                            });
-                          },
-                        ),
-                      ),
-                      const SizedBox(width: 8.0),
-                      GestureDetector(
-                        onTap: () {
-                          if (pairs.length == 1) {
-                            Navigator.of(context).maybePop(null);
-                          } else {
-                            setState(() {
-                              pairs.removeAt(i);
-                            });
-                          }
-                        },
-                        child: const Icon(
-                          Icons.close,
-                          size: 20.0,
-                        ),
-                      ),
+                      for (var i = 0; i < pairs.length; i++)
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 4.0),
+                          child: Row(
+                            children: [
+                              Icon(
+                                duplicateds[i]
+                                    ? Icons.error_outline
+                                    : Icons.check_circle_outline,
+                                color:
+                                    duplicateds[i] ? Colors.red : Colors.green,
+                              ),
+                              const SizedBox(width: 8.0),
+                              Expanded(
+                                child: cangyan.EditableText(
+                                  pairs[i].$1,
+                                  onSubmitted: (text) {
+                                    setState(() {
+                                      pairs[i] = (text, pairs[i].$2);
+                                    });
+                                  },
+                                ),
+                              ),
+                              const SizedBox(width: 8.0),
+                              GestureDetector(
+                                onTap: () {
+                                  if (pairs.length == 1) {
+                                    Navigator.of(context).maybePop(null);
+                                  } else {
+                                    setState(() {
+                                      pairs.removeAt(i);
+                                    });
+                                  }
+                                },
+                                child: const Icon(
+                                  Icons.close,
+                                  size: 20.0,
+                                ),
+                              ),
+                            ],
+                          ),
+                        )
                     ],
                   ),
-                )
-            ],
-          ),
+                ),
+              ),
+            ),
+            const Divider(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                TextButton(
+                  style: const ButtonStyle(
+                    minimumSize: WidgetStatePropertyAll(Size.zero),
+                    tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                    padding: WidgetStatePropertyAll(EdgeInsets.symmetric(
+                      horizontal: 16.0,
+                      vertical: 8.0,
+                    )),
+                  ),
+                  onPressed: () {
+                    Navigator.of(context).maybePop(null);
+                  },
+                  child: const Text('取消'),
+                ),
+                TextButton(
+                  style: const ButtonStyle(
+                    minimumSize: WidgetStatePropertyAll(Size.zero),
+                    tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                    padding: WidgetStatePropertyAll(EdgeInsets.symmetric(
+                      horizontal: 16.0,
+                      vertical: 8.0,
+                    )),
+                  ),
+                  onPressed: () {
+                    Navigator.of(context).maybePop(pairs);
+                  },
+                  child: const Text('导入'),
+                ),
+              ],
+            )
+          ],
         ),
       ),
-      actions: [
-        TextButton(
-          onPressed: () {
-            Navigator.of(context).maybePop(null);
-          },
-          child: const Text('取消'),
-        ),
-        TextButton(
-          onPressed: () {
-            Navigator.of(context).maybePop(pairs);
-          },
-          child: const Text('导入'),
-        ),
-      ],
+      // title: const Text('工程重名'),
+      // contentPadding: const EdgeInsets.symmetric(
+      //   horizontal: 24.0,
+      //   vertical: 8.0,
+      // ),
+      // content: ConstrainedBox(
+      //   constraints: const BoxConstraints(
+      //     maxHeight: 128.0,
+      //   ),
+      //   child: SingleChildScrollView(
+      //     child: Column(
+      //       mainAxisSize: MainAxisSize.min,
+      //       children: [
+      //         for (var i = 0; i < pairs.length; i++)
+      //           Padding(
+      //             padding: const EdgeInsets.symmetric(vertical: 4.0),
+      //             child: Row(
+      //               children: [
+      //                 Icon(
+      //                   duplicateds[i]
+      //                       ? Icons.error_outline
+      //                       : Icons.check_circle_outline,
+      //                   color: duplicateds[i] ? Colors.red : Colors.green,
+      //                 ),
+      //                 const SizedBox(width: 8.0),
+      //                 Expanded(
+      //                   child: cangyan.EditableText(
+      //                     pairs[i].$1,
+      //                     onSubmitted: (text) {
+      //                       setState(() {
+      //                         pairs[i] = (text, pairs[i].$2);
+      //                       });
+      //                     },
+      //                   ),
+      //                 ),
+      //                 const SizedBox(width: 8.0),
+      //                 GestureDetector(
+      //                   onTap: () {
+      //                     if (pairs.length == 1) {
+      //                       Navigator.of(context).maybePop(null);
+      //                     } else {
+      //                       setState(() {
+      //                         pairs.removeAt(i);
+      //                       });
+      //                     }
+      //                   },
+      //                   child: const Icon(
+      //                     Icons.close,
+      //                     size: 20.0,
+      //                   ),
+      //                 ),
+      //               ],
+      //             ),
+      //           )
+      //       ],
+      //     ),
+      //   ),
+      // ),
+      // actions: [
+      //   TextButton(
+      //     onPressed: () {
+      //       Navigator.of(context).maybePop(null);
+      //     },
+      //     child: const Text('取消'),
+      //   ),
+      //   TextButton(
+      //     onPressed: () {
+      //       Navigator.of(context).maybePop(pairs);
+      //     },
+      //     child: const Text('导入'),
+      //   ),
+      // ],
     );
   }
 }
