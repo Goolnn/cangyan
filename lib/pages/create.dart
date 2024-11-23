d1399aecd9f18c2c0e8ca5869ec0af97f45c7d3d
Goolnn <Goolnn@hotmail.com>
2024-11-23 20:44:49 +0800

    feat(pages/create): change image storage type to MemoryImage and enhance image handling features

--- lib/pages/create.dart
+++ lib/pages/create.dart
@@ -22,7 +22,7 @@ class _CreatePageState extends State<CreatePage> {
 
   final controller = TextEditingController();
 
-  final images = <Uint8List>[];
+  final images = <MemoryImage>[];
 
   @override
   Widget build(BuildContext context) {
@@ -59,76 +59,122 @@ class _CreatePageState extends State<CreatePage> {
               ),
               const SizedBox(height: 8.0),
               Expanded(
-                child: Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Colors.blueGrey,
-                      width: 1.0,
-                    ),
-                    borderRadius: BorderRadius.circular(8.0),
+                  child: Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.blueGrey,
+                    width: 1.0,
                   ),
-                  child: Stack(
-                    children: [
-                      SizedBox(
-                        width: double.infinity,
-                        child: SingleChildScrollView(
-                          child: Wrap(
-                            children: [
-                              for (int i = 0; i < images.length; i++)
-                                FractionallySizedBox(
-                                  widthFactor: 1 / (Platform.isAndroid ? 3 : 5),
-                                  child: Padding(
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: Column(
-                                      children: [
-                                        AspectRatio(
-                                          aspectRatio: 3.0 / 4.0,
-                                          child: cangyan.Image(
-                                            provider: MemoryImage(images[i]),
-                                          ),
-                                        ),
-                                        const SizedBox(height: 4.0),
-                                        Text('第${i + 1}页'),
-                                      ],
-                                    ),
-                                  ),
-                                )
-                            ],
-                          ),
-                        ),
-                      ),
-                      Align(
-                        alignment: Alignment.bottomRight,
-                        child: Padding(
-                          padding: EdgeInsets.all(
-                            Platform.isAndroid ? 8.0 : 16.0,
-                          ),
-                          child: FloatingActionButton.small(
-                            shape: const CircleBorder(),
-                            onPressed: () async {
-                              final images =
-                                  (await platform.invokeListMethod("images"))
-                                      ?.map((image) => image as Uint8List)
-                                      .toList();
-
-                              if (images == null) {
-                                return;
-                              }
-
-                              for (final image in images) {
-                                setState(() {
-                                  this.images.add(image);
-                                });
-                              }
-                            },
-                            child: const Icon(Icons.add),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
+                  borderRadius: BorderRadius.circular(8.0),
                 ),
-              ),
+                child: images.isEmpty
+                    ? GestureDetector(
+                        behavior: HitTestBehavior.translucent,
+                        onTap: () {
+                          final images = platform.invokeListMethod("images");
+
+                          images.then((images) {
+                            if (images == null) {
+                              return;
+                            }
+
+                            for (final image in images) {
+                              setState(() {
+                                this.images.add(MemoryImage(image));
+                              });
+                            }
+                          });
+                        },
+                        child: const Center(
+                          child: Icon(
+                            Icons.add,
+                            size: 32.0,
+                          ),
+                        ),
+                      )
+                    : cangyan.ImageViewer(
+                        onReorder: (from, to) {
+                          final image = images.removeAt(from);
+
+                          images.insert(to, image);
+                        },
+                        children: [
+                          for (int i = 0; i < images.length; i++)
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: cangyan.Wave(
+                                borderRadius: BorderRadius.circular(8.0),
+                                onTapUp: (details) {
+                                  HapticFeedback.vibrate();
+
+                                  showMenu<int>(
+                                    context: context,
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(
+                                        8.0,
+                                      ),
+                                    ),
+                                    menuPadding: const EdgeInsets.all(4.0),
+                                    position: RelativeRect.fromLTRB(
+                                      details.globalPosition.dx,
+                                      details.globalPosition.dy,
+                                      details.globalPosition.dx,
+                                      details.globalPosition.dy,
+                                    ),
+                                    items: [
+                                      const PopupMenuItem(
+                                        value: 0,
+                                        height: 36.0,
+                                        child: Text('向前插入新页'),
+                                      ),
+                                      const PopupMenuItem(
+                                        value: 1,
+                                        height: 36.0,
+                                        child: Text('向后插入新页'),
+                                      ),
+                                      const PopupMenuDivider(),
+                                      const PopupMenuItem(
+                                        value: 2,
+                                        height: 36.0,
+                                        child: Text('删除选择页'),
+                                      ),
+                                    ],
+                                  ).then((value) async {
+                                    if (value == 0 || value == 1) {
+                                      final images = platform.invokeListMethod(
+                                        "images",
+                                      );
+
+                                      images.then((images) {
+                                        images?.reversed.forEach((image) {
+                                          if (image is Uint8List) {
+                                            setState(() {
+                                              this.images.insert(
+                                                    value == 0 ? i : i + 1,
+                                                    MemoryImage(image),
+                                                  );
+                                            });
+                                          }
+                                        });
+                                      });
+                                    }
+
+                                    if (value case 2) {
+                                      setState(() => images.removeAt(i));
+                                    }
+                                  });
+                                },
+                                child: AspectRatio(
+                                  aspectRatio: 3.0 / 4.0,
+                                  child: cangyan.Image(
+                                    provider: images[i],
+                                  ),
+                                ),
+                              ),
+                            ),
+                        ],
+                      ),
+              )),
               const Divider(),
               Align(
                 alignment: Alignment.centerRight,
@@ -149,7 +195,9 @@ class _CreatePageState extends State<CreatePage> {
                           : () {
                               final summary = widget.workspace.create(
                                 title: controller.text,
-                                images: images,
+                                images: images.map((image) {
+                                  return image.bytes;
+                                }).toList(),
                               );
 
                               Navigator.of(context).pop(summary);
