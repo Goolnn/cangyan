23d967843dc3c835d54c2fded80d2975c11c2114
Goolnn <Goolnn@hotmail.com>
2024-11-27 18:23:15 +0800

    feat(core/config): add empty method to Config struct for default initialization

--- core/src/api/config/mod.rs
+++ core/src/api/config/mod.rs
@@ -2,17 +2,26 @@ use flutter_rust_bridge::frb;
 use serde::Deserialize;
 use serde::Serialize;
 
-#[derive(Debug, Default, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 #[frb(non_opaque)]
 pub struct Config {
-    #[frb[non_final]]
+    #[frb(non_final)]
     pub workspace: Option<String>,
 
-    #[frb[non_final]]
+    #[frb(non_final)]
     pub preview_features: bool,
 }
 
 impl Config {
+    #[frb(sync)]
+    pub fn empty() -> Self {
+        Config {
+            workspace: None,
+
+            preview_features: false,
+        }
+    }
+
     #[frb(sync)]
     pub fn new(value: String) -> anyhow::Result<Self> {
         let config: Config = serde_yaml::from_str(&value)?;
