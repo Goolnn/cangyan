d1dafd7f6a6cc453a2f61c25c272dfc29491aee5
Goolnn <Goolnn@hotmail.com>
2024-11-27 17:25:53 +0800

    feat(core/config): implement new methods for Config struct to handle YAML serialization

--- core/src/api/config/mod.rs
+++ core/src/api/config/mod.rs
@@ -7,3 +7,17 @@ pub struct Config {
 
     pub preview_features: bool,
 }
+
+impl Config {
+    pub fn new(value: String) -> anyhow::Result<Self> {
+        let config: Config = serde_yaml::from_str(&value)?;
+
+        Ok(config)
+    }
+
+    pub fn to_string(&self) -> anyhow::Result<String> {
+        let yaml = serde_yaml::to_string(&self)?;
+
+        Ok(yaml)
+    }
+}
