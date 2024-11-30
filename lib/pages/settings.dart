0f901e91382619331532a7dc0c6f9eb7731235fd
Goolnn <Goolnn@hotmail.com>
2024-11-30 12:04:37 +0800

    feat(pages/settings): convert SettingsPage to StatefulWidget and add update check functionality

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -1,13 +1,54 @@
 import 'package:flutter/material.dart';
 
-class SettingsPage extends StatelessWidget {
+class SettingsPage extends StatefulWidget {
   const SettingsPage({super.key});
 
   @override
+  State<SettingsPage> createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  bool checkUpdate = true;
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SafeArea(
-        child: Placeholder(),
+        child: Column(
+          children: [
+            GestureDetector(
+              behavior: HitTestBehavior.translucent,
+              onTap: () {
+                setState(() {
+                  checkUpdate = !checkUpdate;
+                });
+              },
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.all(12.0),
+                    child: Text('自动检查最新版本'),
+                  ),
+                  Checkbox(
+                    value: checkUpdate,
+                    onChanged: (value) {
+                      setState(() {
+                        checkUpdate = value!;
+                      });
+                    },
+                  ),
+                ],
+              ),
+            ),
+            Center(
+              child: FilledButton(
+                onPressed: () {},
+                child: const Text('检查更新'),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
