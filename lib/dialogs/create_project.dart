4cdb44caf2ddf0553d76fb96d3015925ba943584
Goolnn <Goolnn@hotmail.com>
2024-11-30 22:02:45 +0800

    feat(dialogs/create_project): adjust container height and replace Expanded with Flexible for better layout management

--- lib/dialogs/create_project.dart
+++ lib/dialogs/create_project.dart
@@ -58,8 +58,7 @@ class _CreateProjectPageState extends State<CreateProjectPage> {
       ),
       child: ConstrainedBox(
         constraints: const BoxConstraints(
-          maxWidth: 720.0,
-          maxHeight: 540.0,
+          maxHeight: 384.0,
         ),
         child: Column(
           mainAxisSize: MainAxisSize.min,
@@ -70,125 +69,129 @@ class _CreateProjectPageState extends State<CreateProjectPage> {
               onChanged: (text) => setState(() {}),
             ),
             const SizedBox(height: 8.0),
-            Expanded(
-                child: Container(
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.blueGrey,
-                  width: 1.0,
+            Flexible(
+              child: Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.blueGrey,
+                    width: 1.0,
+                  ),
+                  borderRadius: BorderRadius.circular(8.0),
                 ),
-                borderRadius: BorderRadius.circular(8.0),
-              ),
-              child: images.isEmpty
-                  ? GestureDetector(
-                      behavior: HitTestBehavior.translucent,
-                      onTap: () {
-                        final images = platform.invokeListMethod("images");
+                child: images.isEmpty
+                    ? GestureDetector(
+                        behavior: HitTestBehavior.translucent,
+                        onTap: () {
+                          final images = platform.invokeListMethod("images");
 
-                        images.then((images) {
-                          if (images == null) {
-                            return;
-                          }
+                          images.then((images) {
+                            if (images == null) {
+                              return;
+                            }
 
-                          for (final image in images) {
-                            setState(() {
-                              this.images.add(MemoryImage(image));
-                            });
-                          }
-                        });
-                      },
-                      child: const Center(
-                        child: Icon(
-                          Icons.add,
-                          size: 32.0,
+                            for (final image in images) {
+                              setState(() {
+                                this.images.add(MemoryImage(image));
+                              });
+                            }
+                          });
+                        },
+                        child: const Center(
+                          child: Icon(
+                            Icons.add,
+                            size: 32.0,
+                          ),
                         ),
-                      ),
-                    )
-                  : cangyan.ImageViewer(
-                      onReorder: (from, to) {
-                        setState(() {
-                          final image = images.removeAt(from);
+                      )
+                    : SingleChildScrollView(
+                        child: cangyan.ImageViewer(
+                          onReorder: (from, to) {
+                            setState(() {
+                              final image = images.removeAt(from);
 
-                          images.insert(to, image);
-                        });
-                      },
-                      children: [
-                        for (int i = 0; i < images.length; i++)
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: cangyan.Wave(
-                              borderRadius: BorderRadius.circular(8.0),
-                              onTapUp: (details) {
-                                HapticFeedback.vibrate();
+                              images.insert(to, image);
+                            });
+                          },
+                          children: [
+                            for (int i = 0; i < images.length; i++)
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: cangyan.Wave(
+                                  borderRadius: BorderRadius.circular(8.0),
+                                  onTapUp: (details) {
+                                    HapticFeedback.vibrate();
 
-                                showMenu<int>(
-                                  context: context,
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(
-                                      8.0,
-                                    ),
-                                  ),
-                                  menuPadding: const EdgeInsets.all(4.0),
-                                  position: RelativeRect.fromLTRB(
-                                    details.globalPosition.dx,
-                                    details.globalPosition.dy,
-                                    details.globalPosition.dx,
-                                    details.globalPosition.dy,
-                                  ),
-                                  items: [
-                                    const PopupMenuItem(
-                                      value: 0,
-                                      height: 36.0,
-                                      child: Text('向前插入新页'),
-                                    ),
-                                    const PopupMenuItem(
-                                      value: 1,
-                                      height: 36.0,
-                                      child: Text('向后插入新页'),
-                                    ),
-                                    const PopupMenuDivider(),
-                                    const PopupMenuItem(
-                                      value: 2,
-                                      height: 36.0,
-                                      child: Text('删除选择页'),
-                                    ),
-                                  ],
-                                ).then((value) async {
-                                  if (value == 0 || value == 1) {
-                                    final images = platform.invokeListMethod(
-                                      "images",
-                                    );
+                                    showMenu<int>(
+                                      context: context,
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(
+                                          8.0,
+                                        ),
+                                      ),
+                                      menuPadding: const EdgeInsets.all(4.0),
+                                      position: RelativeRect.fromLTRB(
+                                        details.globalPosition.dx,
+                                        details.globalPosition.dy,
+                                        details.globalPosition.dx,
+                                        details.globalPosition.dy,
+                                      ),
+                                      items: [
+                                        const PopupMenuItem(
+                                          value: 0,
+                                          height: 36.0,
+                                          child: Text('向前插入新页'),
+                                        ),
+                                        const PopupMenuItem(
+                                          value: 1,
+                                          height: 36.0,
+                                          child: Text('向后插入新页'),
+                                        ),
+                                        const PopupMenuDivider(),
+                                        const PopupMenuItem(
+                                          value: 2,
+                                          height: 36.0,
+                                          child: Text('删除选择页'),
+                                        ),
+                                      ],
+                                    ).then((value) async {
+                                      if (value == 0 || value == 1) {
+                                        final images =
+                                            platform.invokeListMethod(
+                                          "images",
+                                        );
 
-                                    images.then((images) {
-                                      images?.reversed.forEach((image) {
-                                        if (image is Uint8List) {
-                                          setState(() {
-                                            this.images.insert(
-                                                  value == 0 ? i : i + 1,
-                                                  MemoryImage(image),
-                                                );
+                                        images.then((images) {
+                                          images?.reversed.forEach((image) {
+                                            if (image is Uint8List) {
+                                              setState(() {
+                                                this.images.insert(
+                                                      value == 0 ? i : i + 1,
+                                                      MemoryImage(image),
+                                                    );
+                                              });
+                                            }
                                           });
-                                        }
-                                      });
-                                    });
-                                  }
+                                        });
+                                      }
 
-                                  if (value case 2) {
-                                    setState(() => images.removeAt(i));
-                                  }
-                                });
-                              },
-                              child: AspectRatio(
-                                aspectRatio: 3.0 / 4.0,
-                                child: cangyan.Image(
-                                  provider: images[i],
+                                      if (value case 2) {
+                                        setState(() => images.removeAt(i));
+                                      }
+                                    });
+                                  },
+                                  child: AspectRatio(
+                                    aspectRatio: 3.0 / 4.0,
+                                    child: cangyan.Image(
+                                      provider: images[i],
+                                    ),
+                                  ),
                                 ),
                               ),
-                            ),
-                          ),
-                      ],
-                    ),
-            )),
+                          ],
+                        ),
+                      ),
+              ),
+            ),
           ],
         ),
       ),
