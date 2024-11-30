80288d6f9b02da5fecc37aaeb6981c0ad388c4ee
Goolnn <Goolnn@hotmail.com>
2024-11-30 16:40:10 +0800

    feat(pages/settings): implement CheckUpdateButton for version checking and update notifications

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -1,4 +1,7 @@
+import 'package:cangyan/core/api/update.dart';
+import 'package:cangyan/dialogs/new_version.dart';
 import 'package:flutter/material.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 class SettingsPage extends StatefulWidget {
   const SettingsPage({super.key});
@@ -9,6 +12,8 @@ class SettingsPage extends StatefulWidget {
 
 class _SettingsPageState extends State<SettingsPage> {
   bool checkUpdate = true;
+
+  bool fetching = false;
 
   @override
   Widget build(BuildContext context) {
@@ -41,15 +46,154 @@ class _SettingsPageState extends State<SettingsPage> {
                 ],
               ),
             ),
-            Center(
-              child: FilledButton(
-                onPressed: () {},
-                child: const Text('检查更新'),
-              ),
+            const Center(
+              child: CheckUpdateButton(),
+              // child: FilledButton(
+              //   onPressed: () {
+              //     if (fetching) {
+              //       return;
+              //     }
+
+              //     setState(() {
+              //       fetching = true;
+              //     });
+
+              //     Update.fetch().then((release) async {
+              //       final info = await PackageInfo.fromPlatform();
+              //       final version = 'v${info.version}';
+
+              //       if (release.checkUpdate(version: version)) {
+              //         print('New version available');
+              //       } else {
+              //         print('No new version available');
+              //       }
+
+              //       setState(() {
+              //         fetching = false;
+              //       });
+              //     });
+              //   },
+              //   child: fetching
+              //       ? const SizedBox.square(
+              //           dimension: 20.0,
+              //           child: CircularProgressIndicator(
+              //             color: Colors.white,
+              //           ),
+              //         )
+              //       : const Text('检查更新'),
+              // ),
             )
           ],
         ),
       ),
+    );
+  }
+}
+
+class CheckUpdateButton extends StatefulWidget {
+  const CheckUpdateButton({super.key});
+
+  @override
+  State<CheckUpdateButton> createState() => _CheckUpdateButtonState();
+}
+
+class _CheckUpdateButtonState extends State<CheckUpdateButton> {
+  bool fetching = false;
+  bool latested = false;
+
+  bool downloading = false;
+  double progress = 0.0;
+
+  String? version;
+
+  Future<bool?> showNewVersionDialog(
+    String currentVersion,
+    String latestVersion,
+    DateTime published,
+    Asset asset,
+  ) {
+    return showDialog<bool>(
+      context: context,
+      builder: (context) {
+        return NewVersionDialog(
+          currentVersion: currentVersion,
+          latestVersion: latestVersion,
+          published: published,
+          asset: asset,
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FilledButton.tonal(
+      style: FilledButton.styleFrom(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+      ),
+      onPressed: fetching || latested
+          ? null
+          : () {
+              if (fetching) {
+                return;
+              }
+
+              setState(() {
+                fetching = true;
+              });
+
+              Update.fetch().then((release) async {
+                final info = await PackageInfo.fromPlatform();
+                const version = 'v0.1.0'; //'v${info.version}';
+
+                if (release.checkUpdate(version: version)) {
+                  Asset? asset;
+
+                  try {
+                    asset = release.assets.firstWhere((asset) {
+                      return asset.name.contains('android');
+                    });
+                  } catch (e) {
+                    asset = null;
+                  }
+
+                  if (asset == null) {
+                    setState(() {
+                      fetching = false;
+                      latested = true;
+                    });
+                  } else {
+                    const currentVersion = version;
+                    final latestVersion = release.version;
+
+                    final dateTime = release.published;
+                    final published = DateTime.parse(dateTime).toLocal();
+
+                    showNewVersionDialog(
+                      currentVersion,
+                      latestVersion,
+                      published,
+                      asset,
+                    ).then((value) {
+                      setState(() {
+                        fetching = false;
+                        latested = false;
+                      });
+                    });
+                  }
+                }
+              });
+            },
+      child: fetching
+          ? const SizedBox.square(
+              dimension: 20.0,
+              child: CircularProgressIndicator(
+                color: Colors.white,
+              ),
+            )
+          : Text(latested ? '已是最新版本' : '检查更新'),
     );
   }
 }
