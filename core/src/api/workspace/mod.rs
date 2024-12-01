34557ac6bd81f8bdc7efe21a879de6aaa190e254
Goolnn <Goolnn@hotmail.com>
2024-12-01 23:24:26 +0800

    feat(core/workspace): enhance Workspace creation with directory validation and error handling

--- core/src/api/workspace/mod.rs
+++ core/src/api/workspace/mod.rs
@@ -1,15 +1,24 @@
 use std::path::PathBuf;
+use thiserror::Error;
 
 pub struct Workspace {
     path: PathBuf,
 }
 
 impl Workspace {
-    pub fn new(path: String) -> Self {
+    pub fn new(path: String) -> anyhow::Result<Self> {
         let path = PathBuf::from(path);
 
-        Self { path }
+        if !path.is_dir() {
+            return Err(Error::PathNotDirectory { path }.into());
+        }
+
+        Ok(Self { path })
     }
 }
 
-pub enum Error {}
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error("\"{}\" is not directory", path.display())]
+    PathNotDirectory { path: PathBuf },
+}
