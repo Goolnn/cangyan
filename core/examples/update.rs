32493a43fa3f2959059c928c2db55e15d0a256b7
Goolnn <Goolnn@hotmail.com>
2024-12-01 23:09:29 +0800

    feat(core/examples/update): display current and latest version with update time in example

--- core/examples/update.rs
+++ core/examples/update.rs
@@ -1,5 +1,8 @@
 use cangyan::api::update::Platform;
 use cangyan::api::update::Update;
+use chrono::DateTime;
+use chrono::Local;
+use chrono::Utc;
 
 const VERSION: &str = "0.1.0";
 
@@ -11,7 +14,16 @@ async fn main() -> anyhow::Result<()> {
     println!();
 
     if release.check_update(VERSION)? {
-        println!("发现新版本: {}", release.version);
+        println!("当前版本: {}", VERSION);
+        println!("最新版本: {}", release.version);
+        println!(
+            "更新时间: {}",
+            release
+                .published
+                .parse::<DateTime<Utc>>()?
+                .with_timezone(&Local)
+                .format("%Y年%m月%d日 %H:%M:%S")
+        );
         println!();
 
         if let Some(asset) = release.asset_of(Platform::Android) {
