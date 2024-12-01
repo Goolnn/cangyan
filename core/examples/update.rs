a59cc6e4fb5b5e8716457ac4db6a6a48551e6f21
Goolnn <Goolnn@hotmail.com>
2024-12-01 23:02:20 +0800

    feat(core/examples/update): enhance update example with asset details for Android platform

--- core/examples/update.rs
+++ core/examples/update.rs
@@ -1,3 +1,4 @@
+use cangyan::api::update::Platform;
 use cangyan::api::update::Update;
 
 const VERSION: &str = "0.1.0";
@@ -7,9 +8,18 @@ async fn main() -> anyhow::Result<()> {
     let release = Update::fetch().await?;
 
     println!("{:#?}", release);
+    println!();
 
     if release.check_update(VERSION)? {
         println!("发现新版本: {}", release.version);
+        println!();
+
+        if let Some(asset) = release.asset_of(Platform::Android) {
+            println!("资源名称：{}", asset.name);
+            println!("资源平台：{:?}", Platform::Android);
+            println!("资源大小：{:.2} MiB", asset.size as f64 / 1024.0 / 1024.0);
+            println!("下载地址：{}", asset.url);
+        }
     } else {
         println!("已是最新版本");
     }
