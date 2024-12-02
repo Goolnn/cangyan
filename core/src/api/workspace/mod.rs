c026b1c37c6dc5e4b7ef3af018e71f7ffc81c2bf
Goolnn <Goolnn@hotmail.com>
2024-12-02 17:00:45 +0800

    feat(core/workspace): extend Workspace struct to include folders and files

--- core/src/api/workspace/mod.rs
+++ core/src/api/workspace/mod.rs
@@ -1,19 +1,48 @@
+mod file;
+mod folder;
+
+pub use file::File;
+pub use folder::Folder;
+
 use std::path::PathBuf;
 use thiserror::Error;
 
 pub struct Workspace {
     path: PathBuf,
+
+    folders: Vec<Folder>,
+    files: Vec<File>,
 }
 
 impl Workspace {
     pub fn new(path: String) -> anyhow::Result<Self> {
         let path = PathBuf::from(path);
 
+        let mut folders = Vec::new();
+        let mut files = Vec::new();
+
         if !path.is_dir() {
             return Err(Error::PathNotDirectory { path }.into());
         }
 
-        Ok(Self { path })
+        for entry in path.read_dir()?.flatten() {
+            let path = entry.path();
+
+            if path.is_dir() {
+                folders.push(Folder::new(path.to_path_buf()));
+            }
+
+            if path.is_file() {
+                files.push(File::new(path.to_path_buf()));
+            }
+        }
+
+        Ok(Self {
+            path,
+
+            folders,
+            files,
+        })
     }
 }
 
