862b1dd6f21a6751e4216f4ad7fbf6b482d01f5c
Goolnn <Goolnn@hotmail.com>
2025-01-11 13:14:43 +0800

    feat(main): enhance main function with platform-specific UI initialization and comments

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,36 @@
+import 'dart:io';
+
 import 'package:cangyan/cangyan.dart' as cangyan;
-import 'package:cangyan/utils/themes.dart' as themes;
 import 'package:cangyan/core/frb_generated.dart';
 import 'package:cangyan/utils/dirs.dart' as dirs;
+import 'package:cangyan/utils/themes.dart' as themes;
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
 Future<void> main() async {
+  // Initialize the Rust backend library
   await RustLib.init();
 
-  WidgetsFlutterBinding.ensureInitialized();
+  // Set the system UI overlay style
+  switch (Platform.operatingSystem) {
+    case "android":
+      WidgetsFlutterBinding.ensureInitialized();
 
-  SystemChrome.setSystemUIOverlayStyle(
-    const SystemUiOverlayStyle(
-      statusBarColor: Colors.transparent,
-      statusBarIconBrightness: Brightness.dark,
-    ),
-  );
+      SystemChrome.setSystemUIOverlayStyle(
+        const SystemUiOverlayStyle(
+          statusBarColor: Colors.transparent,
+          statusBarIconBrightness: Brightness.dark,
+        ),
+      );
 
+      break;
+  }
+
+  // Get the workspace path
   final path = await dirs.workspace();
 
+  // Run the app
   runApp(
     MaterialApp(
       title: "苍眼",
