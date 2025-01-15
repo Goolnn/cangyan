32b55b487f8ff35b0c9fbfa834769555cf3e9b00
Goolnn <Goolnn@hotmail.com>
2025-01-15 16:45:53 +0800

    feat: add support for image picking on Windows platform

--- lib/utils/picker.dart
+++ lib/utils/picker.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -6,13 +9,30 @@ const platform = MethodChannel('com.goolnn.cangyan/picker');
 Future<void> pickProjects() async {}
 
 Future<List<MemoryImage>> pickImages() async {
-  return platform
-      .invokeListMethod<Uint8List>("images")
-      .then<List<Uint8List>>((images) {
-    return images ?? [];
-  }).then((images) {
-    return images.map((image) {
-      return MemoryImage(image);
-    }).toList();
-  });
+  switch (Platform.operatingSystem) {
+    case "android":
+      return platform
+          .invokeListMethod<Uint8List>("images")
+          .then<List<Uint8List>>((images) {
+        return images ?? [];
+      }).then((images) {
+        return images.map((image) {
+          return MemoryImage(image);
+        }).toList();
+      });
+    case "windows":
+      FilePickerResult? result = await FilePicker.platform.pickFiles(
+        allowMultiple: true,
+        type: FileType.custom,
+        allowedExtensions: ['jpg', 'png'],
+      );
+
+      if (result != null) {
+        return result.paths.map((path) {
+          return MemoryImage(File(path!).readAsBytesSync());
+        }).toList();
+      }
+  }
+
+  return [];
 }
