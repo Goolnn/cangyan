291e454d81f5f40686ae2dbc72a76bd307ca0878
Goolnn <Goolnn@hotmail.com>
2025-01-20 11:49:47 +0800

    feat(dialogs/standard_dialog): adjust aspect ratio based on platform for improved responsiveness

--- lib/dialogs/standard_dialog.dart
+++ lib/dialogs/standard_dialog.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 class StandardDialog extends StatelessWidget {
@@ -20,29 +22,41 @@ class StandardDialog extends StatelessWidget {
           maxWidth: 800.0,
           maxHeight: 600.0,
         ),
-        child: AspectRatio(
-          aspectRatio: 4.0 / 3.0,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Center(
-                  child: Text(
-                    title,
-                    style: const TextStyle(
-                      fontSize: 20.0,
+        child: Builder(builder: (context) {
+          final double aspectRatio;
+
+          switch (Platform.operatingSystem) {
+            case 'android':
+              aspectRatio = 3.0 / 4.5;
+              break;
+            default:
+              aspectRatio = 4.0 / 3.0;
+          }
+
+          return AspectRatio(
+            aspectRatio: aspectRatio,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Center(
+                    child: Text(
+                      title,
+                      style: const TextStyle(
+                        fontSize: 20.0,
+                      ),
                     ),
                   ),
-                ),
-                const Divider(),
-                child,
-                const Divider(),
-                action,
-              ],
+                  const Divider(),
+                  child,
+                  const Divider(),
+                  action,
+                ],
+              ),
             ),
-          ),
-        ),
+          );
+        }),
       ),
     );
   }
