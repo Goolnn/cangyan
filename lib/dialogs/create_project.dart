e30f6d4b27e77b39b45000eb8a5dbb261eaa23d0
Goolnn <Goolnn@hotmail.com>
2025-01-20 11:40:44 +0800

    feat(dialogs/create_project): replace Flexible with Expanded for better layout management

--- lib/dialogs/create_project.dart
+++ lib/dialogs/create_project.dart
@@ -57,146 +57,137 @@ class _CreateProjectPageState extends State<CreateProjectPage> {
           ),
         ],
       ),
-      child: Flexible(
-        child: ConstrainedBox(
-          constraints: const BoxConstraints(
-            maxHeight: 384.0,
-          ),
-          child: Column(
-            children: [
-              cangyan.Input(
-                controller: controller,
-                placeholder: "标题",
-                onChanged: (text) => setState(() {}),
-              ),
-              const SizedBox.square(dimension: 8.0),
-              Flexible(
-                child: Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Colors.blueGrey,
-                      width: 1.0,
-                    ),
-                    borderRadius: BorderRadius.circular(8.0),
+      child: Expanded(
+        child: Column(
+          children: [
+            cangyan.Input(
+              controller: controller,
+              placeholder: "标题",
+              onChanged: (text) => setState(() {}),
+            ),
+            const SizedBox.square(dimension: 8.0),
+            Flexible(
+              child: Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.blueGrey,
+                    width: 1.0,
                   ),
-                  child: images.isEmpty
-                      ? GestureDetector(
-                          behavior: HitTestBehavior.translucent,
-                          onTap: () {
-                            pickImages().then((images) {
-                              setState(() {
-                                this.images.addAll(images);
-                              });
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                child: images.isEmpty
+                    ? GestureDetector(
+                        behavior: HitTestBehavior.translucent,
+                        onTap: () {
+                          pickImages().then((images) {
+                            setState(() {
+                              this.images.addAll(images);
                             });
-                          },
-                          child: const Center(
-                            child: Icon(
-                              Icons.add,
-                              size: 32.0,
-                            ),
+                          });
+                        },
+                        child: const Center(
+                          child: Icon(
+                            Icons.add,
+                            size: 32.0,
                           ),
-                        )
-                      : Align(
-                          alignment: Alignment.topLeft,
-                          child: SingleChildScrollView(
-                            child: Builder(builder: (context) {
-                              final int count;
+                        ),
+                      )
+                    : Align(
+                        alignment: Alignment.topLeft,
+                        child: SingleChildScrollView(
+                          child: Builder(builder: (context) {
+                            final int count;
 
-                              switch (Platform.operatingSystem) {
-                                case 'windows':
-                                  count = 4;
-                                  break;
-                                default:
-                                  count = 3;
-                                  break;
-                              }
+                            switch (Platform.operatingSystem) {
+                              case 'windows':
+                                count = 4;
+                                break;
+                              default:
+                                count = 3;
+                                break;
+                            }
 
-                              return cangyan.ImageViewer(
-                                count: count,
-                                onReorder: (from, to) {
-                                  setState(() {
-                                    final image = images.removeAt(from);
+                            return cangyan.ImageViewer(
+                              count: count,
+                              onReorder: (from, to) {
+                                setState(() {
+                                  final image = images.removeAt(from);
 
-                                    images.insert(to, image);
-                                  });
-                                },
-                                children: [
-                                  for (int i = 0; i < images.length; i++)
-                                    Padding(
-                                      padding: const EdgeInsets.all(8.0),
-                                      child: cangyan.Wave(
-                                        borderRadius:
-                                            BorderRadius.circular(8.0),
-                                        onTapUp: (details) {
-                                          HapticFeedback.vibrate();
+                                  images.insert(to, image);
+                                });
+                              },
+                              children: [
+                                for (int i = 0; i < images.length; i++)
+                                  Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: cangyan.Wave(
+                                      borderRadius: BorderRadius.circular(8.0),
+                                      onTapUp: (details) {
+                                        HapticFeedback.vibrate();
 
-                                          showMenu<int>(
-                                            context: context,
-                                            position: RelativeRect.fromLTRB(
-                                              details.globalPosition.dx,
-                                              details.globalPosition.dy,
-                                              details.globalPosition.dx,
-                                              details.globalPosition.dy,
+                                        showMenu<int>(
+                                          context: context,
+                                          position: RelativeRect.fromLTRB(
+                                            details.globalPosition.dx,
+                                            details.globalPosition.dy,
+                                            details.globalPosition.dx,
+                                            details.globalPosition.dy,
+                                          ),
+                                          items: [
+                                            const cangyan.PopupMenuItem(
+                                              value: 0,
+                                              child: Text('向前插入新页'),
                                             ),
-                                            items: [
-                                              const cangyan.PopupMenuItem(
-                                                value: 0,
-                                                child: Text('向前插入新页'),
-                                              ),
-                                              const cangyan.PopupMenuItem(
-                                                value: 1,
-                                                child: Text('向后插入新页'),
-                                              ),
-                                              const cangyan.PopupMenuDivider(),
-                                              const cangyan.PopupMenuItem(
-                                                value: 2,
-                                                child: Text(
-                                                  '删除选择页',
-                                                  style: TextStyle(
-                                                    color: Colors.red,
-                                                  ),
+                                            const cangyan.PopupMenuItem(
+                                              value: 1,
+                                              child: Text('向后插入新页'),
+                                            ),
+                                            const cangyan.PopupMenuDivider(),
+                                            const cangyan.PopupMenuItem(
+                                              value: 2,
+                                              child: Text(
+                                                '删除选择页',
+                                                style: TextStyle(
+                                                  color: Colors.red,
                                                 ),
                                               ),
-                                            ],
-                                          ).then((value) async {
-                                            if (value == 0 || value == 1) {
-                                              pickImages().then((images) {
-                                                for (var image in images) {
-                                                  setState(() {
-                                                    this.images.insert(
-                                                          value == 0
-                                                              ? i
-                                                              : i + 1,
-                                                          image,
-                                                        );
-                                                  });
-                                                }
-                                              });
-                                            }
+                                            ),
+                                          ],
+                                        ).then((value) async {
+                                          if (value == 0 || value == 1) {
+                                            pickImages().then((images) {
+                                              for (var image in images) {
+                                                setState(() {
+                                                  this.images.insert(
+                                                        value == 0 ? i : i + 1,
+                                                        image,
+                                                      );
+                                                });
+                                              }
+                                            });
+                                          }
 
-                                            if (value case 2) {
-                                              setState(
-                                                  () => images.removeAt(i));
-                                            }
-                                          });
-                                        },
-                                        child: AspectRatio(
-                                          aspectRatio: 3.0 / 4.0,
-                                          child: cangyan.Image(
-                                            provider: images[i],
-                                          ),
+                                          if (value case 2) {
+                                            setState(() => images.removeAt(i));
+                                          }
+                                        });
+                                      },
+                                      child: AspectRatio(
+                                        aspectRatio: 3.0 / 4.0,
+                                        child: cangyan.Image(
+                                          provider: images[i],
                                         ),
                                       ),
                                     ),
-                                ],
-                              );
-                            }),
-                          ),
+                                  ),
+                              ],
+                            );
+                          }),
                         ),
-                ),
+                      ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
