450987568832ca9c861c0fdd2e919a25cd78c7bc
Goolnn <Goolnn@hotmail.com>
2025-02-09 22:44:59 +0800

    feat(dependencies): add file_picker plugin and update package versions for compatibility

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,6 +7,7 @@ import Foundation
 
 import app_installer
 import desktop_drop
+import file_picker
 import package_info_plus
 import path_provider_foundation
 import screen_retriever_macos
@@ -16,6 +17,7 @@ import window_manager
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AppInstallerPlugin.register(with: registry.registrar(forPlugin: "AppInstallerPlugin"))
   DesktopDropPlugin.register(with: registry.registrar(forPlugin: "DesktopDropPlugin"))
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   ScreenRetrieverMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverMacosPlugin"))
