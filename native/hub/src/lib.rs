c2a456b6567f9b7ab4e3b87a1069ec2d8eb64edc
Goolnn <Goolnn@hotmail.com>
2025-11-15 18:30:02 +0800

    feat: implement workspace management for Windows and Android

--- native/hub/src/lib.rs
+++ native/hub/src/lib.rs
@@ -1,8 +1,11 @@
 mod actors;
 mod signals;
+mod workspace;
 
+use crate::workspace::Workspace;
 use actors::create_actors;
 use rinf::dart_shutdown;
+use rinf::debug_print;
 use rinf::write_interface;
 use tokio::spawn;
 
@@ -11,6 +14,10 @@ write_interface!();
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
     spawn(create_actors());
+
+    if let Some(workspace) = Workspace::new() {
+        debug_print!("Workspace path: {}", workspace.path().display());
+    }
 
     dart_shutdown().await;
 }
---
+++ native/hub/src/workspace.rs
@@ -0,0 +1,123 @@
+#[cfg(target_os = "windows")]
+mod inner {
+    use directories::UserDirs;
+    use std::path::PathBuf;
+
+    pub fn workspace() -> Option<PathBuf> {
+        let dirs = UserDirs::new()?;
+        let path = dirs.document_dir()?;
+        let buf = path.join("Cangyan");
+
+        if !buf.exists() {
+            std::fs::create_dir_all(&buf).ok()?;
+        }
+
+        Some(buf)
+    }
+}
+
+#[cfg(target_os = "android")]
+mod inner {
+    use jni::JavaVM;
+    use jni::objects::GlobalRef;
+    use jni::objects::JString;
+    use jni::objects::JValue;
+    use jni::sys::JNI_VERSION_1_6;
+    use jni::sys::jint;
+    use std::ffi::c_void;
+    use std::path::PathBuf;
+    use std::sync::OnceLock;
+
+    fn android_context() -> Option<(&'static JavaVM, &'static GlobalRef)> {
+        static VM: OnceLock<JavaVM> = OnceLock::new();
+        static CONTEXT: OnceLock<GlobalRef> = OnceLock::new();
+
+        #[unsafe(no_mangle)]
+        extern "system" fn JNI_OnLoad(vm: JavaVM, _: *mut c_void) -> jint {
+            if let Ok(mut env) = vm.get_env()
+                && let Ok(activity) = env.find_class("android/app/ActivityThread")
+                && let Ok(value) = env.call_static_method(
+                    activity,
+                    "currentActivityThread",
+                    "()Landroid/app/ActivityThread;",
+                    &[],
+                )
+                && let Ok(thread) = value.l()
+                && let Ok(value) =
+                    env.call_method(thread, "getApplication", "()Landroid/app/Application;", &[])
+                && let Ok(application) = value.l()
+                && let Ok(context) = env.new_global_ref(application)
+            {
+                let _ = VM.set(vm);
+                let _ = CONTEXT.set(context);
+            }
+
+            JNI_VERSION_1_6
+        }
+
+        let vm = VM.get()?;
+        let context = CONTEXT.get()?;
+
+        Some((vm, context))
+    }
+
+    pub fn workspace() -> Option<PathBuf> {
+        let (vm, _) = android_context()?;
+
+        let mut env = vm.attach_current_thread().ok()?;
+
+        let environment = env.find_class("android/os/Environment").ok()?;
+
+        let directory = env
+            .get_static_field(&environment, "DIRECTORY_DOCUMENTS", "Ljava/lang/String;")
+            .ok()?
+            .l()
+            .ok()?;
+
+        let file = env
+            .call_static_method(
+                &environment,
+                "getExternalStoragePublicDirectory",
+                "(Ljava/lang/String;)Ljava/io/File;",
+                &[JValue::Object(&directory)],
+            )
+            .ok()?
+            .l()
+            .ok()?;
+
+        let path = env
+            .call_method(file, "getAbsolutePath", "()Ljava/lang/String;", &[])
+            .ok()?
+            .l()
+            .ok()?;
+
+        let buf = PathBuf::from(env.get_string(&JString::from(path)).ok()?.to_str().ok()?)
+            .join("Cangyan");
+
+        if !buf.exists() {
+            std::fs::create_dir_all(&buf).ok()?;
+        }
+
+        Some(buf)
+    }
+}
+
+use std::path::Path;
+use std::path::PathBuf;
+
+#[derive(Debug)]
+pub struct Workspace {
+    path: PathBuf,
+}
+
+impl Workspace {
+    pub fn new() -> Option<Self> {
+        let path = inner::workspace()?;
+
+        Some(Self { path })
+    }
+
+    pub fn path(&self) -> &Path {
+        &self.path
+    }
+}
