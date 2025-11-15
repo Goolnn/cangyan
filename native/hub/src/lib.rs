96618fa95ba58925bc267b5ede1c48d2f59d21c4
Goolnn <Goolnn@hotmail.com>
2025-11-15 18:59:37 +0800

    feat: remove unused actor and signal implementations

--- native/hub/src/lib.rs
+++ native/hub/src/lib.rs
@@ -1,23 +1,12 @@
-mod actors;
-mod signals;
 mod workspace;
 
 use crate::workspace::Workspace;
-use actors::create_actors;
-use rinf::dart_shutdown;
-use rinf::debug_print;
-use rinf::write_interface;
-use tokio::spawn;
 
-write_interface!();
+rinf::write_interface!();
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
-    spawn(create_actors());
+    let _workspace = Workspace::new();
 
-    if let Some(workspace) = Workspace::new() {
-        debug_print!("Workspace path: {}", workspace.path().display());
-    }
-
-    dart_shutdown().await;
+    rinf::dart_shutdown().await;
 }
