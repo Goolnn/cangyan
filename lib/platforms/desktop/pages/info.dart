26d2996e5d06d6e75b663088c3755b23cf443baa
Goolnn <Goolnn@hotmail.com>
2025-11-18 15:48:15 +0800

    feat: enhance InfoPage layout with improved comment display and date information

--- lib/platforms/desktop/pages/info.dart
+++ lib/platforms/desktop/pages/info.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:cangyan/platforms/desktop/window/button.dart' as window;
 import 'package:cangyan/platforms/desktop/window/page.dart' as window;
 import 'package:cangyan/src/bindings/bindings.dart';
@@ -50,13 +52,125 @@ class _InfoPageState extends State<InfoPage> {
       ),
 
       child: Material(
-        child: Center(
-          child: Hero(
-            tag: 'cover_${widget.title}',
-            child: ClipRSuperellipse(
-              borderRadius: BorderRadius.circular(12.0),
-              child: widget.cover,
-            ),
+        child: SingleChildScrollView(
+          padding: EdgeInsets.all(12.0),
+
+          child: LayoutBuilder(
+            builder: (context, constraints) {
+              return Column(
+                children: [
+                  SizedBox(
+                    width: 1024.0 + 512.0,
+                    height: min(constraints.maxWidth / 2.0, 512.0 + 64.0),
+
+                    child: Row(
+                      mainAxisSize: MainAxisSize.max,
+
+                      spacing: 8.0,
+
+                      children: [
+                        AspectRatio(
+                          aspectRatio: 3.0 / 4.0,
+
+                          child: Center(
+                            child: Hero(
+                              tag: 'cover_${widget.title}',
+
+                              child: ClipRSuperellipse(
+                                borderRadius: BorderRadius.circular(12.0),
+                                child: widget.cover,
+                              ),
+                            ),
+                          ),
+                        ),
+
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+
+                            spacing: 8.0,
+
+                            children: [
+                              Expanded(
+                                child: widget.comment.isNotEmpty
+                                    ? SingleChildScrollView(
+                                        padding: const EdgeInsets.only(
+                                          right: 12.0,
+                                        ),
+                                        child: Text(
+                                          widget.comment,
+                                          style: TextStyle(fontSize: 12.0),
+                                        ),
+                                      )
+                                    : Center(
+                                        child: Tooltip(
+                                          message: '添加简介',
+
+                                          waitDuration: Duration(
+                                            milliseconds: 500,
+                                          ),
+
+                                          mouseCursor: SystemMouseCursors.click,
+
+                                          child: Column(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.center,
+                                            mainAxisSize: MainAxisSize.min,
+
+                                            children: [
+                                              Icon(
+                                                Icons.edit_note,
+                                                size: 128.0,
+                                                color: Colors.grey,
+                                              ),
+
+                                              Text(
+                                                '暂无简介',
+                                                style: TextStyle(
+                                                  fontSize: 16.0,
+                                                  color: Colors.grey,
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                      ),
+                              ),
+
+                              Align(
+                                alignment: Alignment.centerRight,
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.end,
+                                  children: [
+                                    Text(
+                                      '创建于 ${widget.createdDate.year}年${widget.createdDate.month}月${widget.createdDate.day}日 ${widget.createdDate.hour}:${widget.createdDate.minute}:${widget.createdDate.second}',
+                                      style: TextStyle(
+                                        fontSize: 10.0,
+                                        color: Colors.grey,
+                                      ),
+                                    ),
+
+                                    Text(
+                                      '修改于 ${widget.updatedDate.year}年${widget.updatedDate.month}月${widget.updatedDate.day}日 ${widget.updatedDate.hour}:${widget.updatedDate.minute}:${widget.updatedDate.second}',
+                                      style: TextStyle(
+                                        fontSize: 10.0,
+                                        color: Colors.grey,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+
+                  Divider(),
+                ],
+              );
+            },
           ),
         ),
       ),
