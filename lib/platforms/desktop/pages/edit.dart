285ab51d10020c91634e3bb1228e664e9dd5baa1
Goolnn <Goolnn@hotmail.com>
2025-11-19 22:34:16 +0800

    feat: refactor EditPage to use StatefulWidget and implement page navigation with buttons

--- lib/platforms/desktop/pages/edit.dart
+++ lib/platforms/desktop/pages/edit.dart
@@ -1,41 +1,142 @@
 import 'package:cangyan/platforms/desktop/window/button.dart' as window;
-import 'package:cangyan/platforms/desktop/window/page.dart' as widgets;
+import 'package:cangyan/platforms/desktop/window/page.dart' as window;
 import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
-class EditPage extends StatelessWidget {
+class EditPage extends StatefulWidget {
   final String path;
 
-  final Widget page;
+  final List<Image> images;
 
-  final int index;
+  final int initialIndex;
 
   const EditPage({
     super.key,
 
     required this.path,
 
-    required this.page,
+    required this.images,
 
-    required this.index,
+    required this.initialIndex,
   });
 
   @override
+  State<EditPage> createState() => _EditPageState();
+}
+
+class _EditPageState extends State<EditPage> {
+  late PageController _controller;
+
+  late int _currentIndex;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = PageController(initialPage: widget.initialIndex);
+
+    _currentIndex = widget.initialIndex;
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return widgets.Page(
+    return window.Page(
       buttons: [window.WindowBackButton(context)],
 
-      child: Material(
-        child: Center(
-          child: Hero(
-            tag: 'page_${path}_$index',
+      panel: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
 
-            child: ClipRSuperellipse(
-              borderRadius: BorderRadius.circular(12.0),
-              child: page,
+          spacing: 8.0,
+
+          children: [
+            Tooltip(
+              message: '上一页',
+
+              waitDuration: Duration(milliseconds: 500),
+
+              child: window.WindowButton(
+                onPressed: _currentIndex > 0
+                    ? () {
+                        _switchPage(-1);
+                      }
+                    : null,
+
+                child: Icon(MdiIcons.chevronLeft),
+              ),
             ),
-          ),
+
+            Text('第${_currentIndex + 1}页'),
+
+            Tooltip(
+              message: '下一页',
+
+              waitDuration: Duration(milliseconds: 500),
+
+              child: window.WindowButton(
+                onPressed: _currentIndex < widget.images.length
+                    ? () {
+                        _switchPage(1);
+                      }
+                    : null,
+
+                child: Icon(MdiIcons.chevronRight),
+              ),
+            ),
+          ],
+        ),
+      ),
+
+      child: Material(
+        child: Stack(
+          children: [
+            PageView.builder(
+              controller: _controller,
+
+              itemCount: widget.images.length,
+
+              onPageChanged: (index) {
+                setState(() {
+                  _currentIndex = index;
+                });
+              },
+
+              itemBuilder: (context, index) {
+                return Center(
+                  child: Hero(
+                    tag: 'page_${widget.path}_$index',
+
+                    child: ClipRSuperellipse(
+                      borderRadius: BorderRadius.circular(12.0),
+
+                      child: widget.images[index],
+                    ),
+                  ),
+                );
+              },
+            ),
+          ],
         ),
       ),
     );
+  }
+
+  void _switchPage(int offset) {
+    int targetPage = _currentIndex + offset;
+
+    if (targetPage >= 0 && targetPage < widget.images.length) {
+      _controller.animateToPage(
+        targetPage,
+        duration: const Duration(milliseconds: 250),
+        curve: Curves.easeInOut,
+      );
+    }
   }
 }
