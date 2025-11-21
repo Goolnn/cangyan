4fbfcfb980c7de5969ed43164a18a8c82f7e06cd
Goolnn <Goolnn@hotmail.com>
2025-11-21 20:17:15 +0800

    feat: add gesture handling to Note widget for improved interactivity

--- lib/platforms/desktop/widgets/note.dart
+++ lib/platforms/desktop/widgets/note.dart
@@ -1,33 +1,53 @@
 import 'package:flutter/material.dart';
 
 class Note extends StatelessWidget {
+  final void Function(DragStartDetails details)? onPanStart;
+  final void Function(DragUpdateDetails details)? onPanUpdate;
+  final void Function(DragEndDetails details)? onPanEnd;
+
   final void Function()? onPressed;
 
   final int index;
 
-  const Note({super.key, this.onPressed, required this.index});
+  const Note({
+    super.key,
+
+    this.onPanStart,
+    this.onPanUpdate,
+    this.onPanEnd,
+
+    this.onPressed,
+
+    required this.index,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return RawMaterialButton(
-      constraints: BoxConstraints.tightFor(width: 32.0, height: 32.0),
+    return GestureDetector(
+      onPanStart: onPanStart,
+      onPanUpdate: onPanUpdate,
+      onPanEnd: onPanEnd,
 
-      shape: OvalBorder(),
+      child: RawMaterialButton(
+        constraints: BoxConstraints.tightFor(width: 32.0, height: 32.0),
 
-      materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+        shape: OvalBorder(),
 
-      fillColor: Colors.red,
-      splashColor: Colors.transparent,
+        materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
 
-      elevation: 0.0,
-      focusElevation: 0.0,
-      hoverElevation: 0.0,
-      disabledElevation: 0.0,
-      highlightElevation: 0.0,
+        fillColor: Colors.red,
+        splashColor: Colors.transparent,
 
-      onPressed: onPressed,
+        elevation: 0.0,
+        focusElevation: 0.0,
+        hoverElevation: 0.0,
+        disabledElevation: 0.0,
+        highlightElevation: 0.0,
 
-      child: Text(index.toString(), style: TextStyle(color: Colors.white)),
+        onPressed: onPressed,
+
+        child: Text(index.toString(), style: TextStyle(color: Colors.white)),
+      ),
     );
   }
 }
